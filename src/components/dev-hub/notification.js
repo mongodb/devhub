ccf296804dfd934059885c325a7146a1cbb0af70
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-02-04 16:45:07 -0500

    Add notification component (without twitch call)

---
+++ src/components/dev-hub/notification.js
@@ -0,0 +1,70 @@
+import React, { useCallback, useState } from 'react';
+import styled from '@emotion/styled';
+import { P } from './text';
+import { colorMap, fontSize, size } from './theme';
+
+const CloseIcon = styled(P)`
+    color: ${colorMap.devWhite};
+    font-size: ${fontSize.micro};
+    margin-left: auto;
+    padding: ${size.default};
+
+    /* TODO replace P with a button here and remove hover CSS */
+    :hover {
+        cursor: pointer;
+    }
+`;
+
+const LiveNowBadgeContainer = styled('div')`
+    border: 1px solid ${colorMap.lightGreen};
+    border-radius: ${size.small};
+    margin-right: ${size.small};
+    padding: ${size.tiny} ${size.small};
+    &:before {
+        /* 25cf is "SMALL DOT" */
+        content: '\u25cf ';
+    }
+`;
+
+const NotificationText = styled('div')`
+    align-items: center;
+    color: ${colorMap.devWhite};
+    display: flex;
+    margin-left: auto;
+`;
+
+const StyledNotification = styled('div')`
+    align-items: center;
+    background-color: ${colorMap.greyDarkTwo};
+    border-radius: ${size.medium};
+    display: flex;
+    justify-content: center;
+`;
+
+const Notification = () => {
+    // TODO add async call to twitch
+    const [isVisible, setIsVisible] = useState(true);
+    const dismissNotification = useCallback(() => setIsVisible(false), [
+        setIsVisible,
+    ]);
+    return (
+        isVisible && (
+            <StyledNotification>
+                <NotificationText>
+                    <LiveNowBadgeContainer>Live Now</LiveNowBadgeContainer>
+                    <P collapse>
+                        Building the world’s first IoT kitty litter box — Join
+                        us on Twitch!
+                    </P>
+                </NotificationText>
+                <CloseIcon collapse onClick={dismissNotification}>
+                    X
+                </CloseIcon>
+            </StyledNotification>
+        )
+    );
+};
+
+Notification.displayName = 'Notification';
+
+export default Notification;
