618caebe7804898a460f5a54cd944db56d0f865c
Gregg Brewster <mail@digitalpuddle.com>
2020-02-04 12:20:09 -0500

    remove docs-tools

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,89 +8,95 @@ const STAGING_SITE = 'docs-mongodbcom-staging.corp.mongodb.com';
 const LOCALHOST_NAMES = ['localhost', '0.0.0.0', '127.0.0.1'];
 
 export default class Navbar extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      activeLink: '',
-    };
+        this.state = {
+            activeLink: '',
+        };
 
-    // Static navprops by default
-    this.navprops = `{"links": [
+        // Static navprops by default
+        this.navprops = `{"links": [
                             {"url": "https://docs.mongodb.com/manual/","text": "Server"},
                             {"url": "https://docs.mongodb.com/ecosystem/drivers/","text": "Drivers"},
                             {"url": "https://docs.mongodb.com/cloud/","text": "Cloud"},
                             {"url": "https://docs.mongodb.com/tools/","text": "Tools"},
                             {"url": "https://docs.mongodb.com/guides/","text": "Guides"}]}`;
-  }
-
-  componentDidMount() {
-    // Add script to give navbar functionality and css
-    const script = document.createElement('script');
-    script.src = withPrefix('docs-tools/navbar.min.js');
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    // Update activeLink state on render
-    if (isBrowser()) {
-      this.setState({ activeLink: this.checkForLink(window.location) });
     }
-  }
 
-  // Uses location to check which link should be active
-  checkForLink = location => {
-    if (location.hostname === DOCS_SITE || location.hostname === STAGING_SITE) {
-      return this.validateActiveLink(location.pathname, '/', URL_SLUGS);
+    componentDidMount() {
+        // Update activeLink state on render
+        if (isBrowser()) {
+            this.setState({ activeLink: this.checkForLink(window.location) });
+        }
     }
-    if (this.isLocalhost(location.hostname)) {
-      const link = this.checkUrlItems(process.env.GATSBY_SITE, URL_SLUGS);
-      return link !== null ? link : this.checkUrlItems(process.env.GATSBY_SITE, URL_SUBDOMAINS);
-    }
-    return this.validateActiveLink(location.hostname, '.', URL_SUBDOMAINS);
-  };
 
-  // Takes the appropriate part of the URL and identifies which link it matches
-  validateActiveLink = (name, token, urlItems) => {
-    const slugs = name.split(token);
-    return this.checkUrlItems(slugs[1], urlItems);
-  };
+    // Uses location to check which link should be active
+    checkForLink = location => {
+        if (
+            location.hostname === DOCS_SITE ||
+            location.hostname === STAGING_SITE
+        ) {
+            return this.validateActiveLink(location.pathname, '/', URL_SLUGS);
+        }
+        if (this.isLocalhost(location.hostname)) {
+            const link = this.checkUrlItems(process.env.GATSBY_SITE, URL_SLUGS);
+            return link !== null
+                ? link
+                : this.checkUrlItems(process.env.GATSBY_SITE, URL_SUBDOMAINS);
+        }
+        return this.validateActiveLink(location.hostname, '.', URL_SUBDOMAINS);
+    };
 
-  checkUrlItems = (slug, urlItems) => {
-    const urlMapping = Object.entries(urlItems).find(([, value]) => value.includes(slug));
-    return urlMapping ? urlMapping[0] : null;
-  };
+    // Takes the appropriate part of the URL and identifies which link it matches
+    validateActiveLink = (name, token, urlItems) => {
+        const slugs = name.split(token);
+        return this.checkUrlItems(slugs[1], urlItems);
+    };
 
-  isLocalhost = hostname => {
-    const found = LOCALHOST_NAMES.find(localhostName => {
-      return localhostName.includes(hostname);
-    });
-    return found !== undefined;
-  };
+    checkUrlItems = (slug, urlItems) => {
+        const urlMapping = Object.entries(urlItems).find(([, value]) =>
+            value.includes(slug)
+        );
+        return urlMapping ? urlMapping[0] : null;
+    };
 
-  isActiveLink = link => {
-    const { activeLink } = this.state;
-    return link.toLowerCase() === activeLink;
-  };
+    isLocalhost = hostname => {
+        const found = LOCALHOST_NAMES.find(localhostName => {
+            return localhostName.includes(hostname);
+        });
+        return found !== undefined;
+    };
 
-  // Modify navprops
-  modifyActiveLink = () => {
-    return `{"links": [
+    isActiveLink = link => {
+        const { activeLink } = this.state;
+        return link.toLowerCase() === activeLink;
+    };
+
+    // Modify navprops
+    modifyActiveLink = () => {
+        return `{"links": [
                     {"url": "https://docs.mongodb.com/manual/","text": "Server", "active": ${this.isActiveLink(
-                      'Server'
+                        'Server'
                     )}},
                     {"url": "https://docs.mongodb.com/ecosystem/drivers/","text": "Drivers", "active": ${this.isActiveLink(
-                      'Drivers'
+                        'Drivers'
                     )}},
-                    {"url": "https://docs.mongodb.com/cloud/","text": "Cloud", "active": ${this.isActiveLink('Cloud')}},
-                    {"url": "https://docs.mongodb.com/tools/","text": "Tools", "active": ${this.isActiveLink('Tools')}},
+                    {"url": "https://docs.mongodb.com/cloud/","text": "Cloud", "active": ${this.isActiveLink(
+                        'Cloud'
+                    )}},
+                    {"url": "https://docs.mongodb.com/tools/","text": "Tools", "active": ${this.isActiveLink(
+                        'Tools'
+                    )}},
                     {"url": "https://docs.mongodb.com/guides/","text": "Guides", "active": ${this.isActiveLink(
-                      'Guides'
+                        'Guides'
                     )}}]}`;
-  };
+    };
 
-  render() {
-    this.navprops = this.modifyActiveLink();
-    return <div id="navbar" className="navbar" data-navprops={this.navprops} />;
-  }
+    render() {
+        this.navprops = this.modifyActiveLink();
+        return (
+            <div id="navbar" className="navbar" data-navprops={this.navprops} />
+        );
+    }
 }
