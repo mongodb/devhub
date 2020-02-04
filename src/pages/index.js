1b4c5fb729f298045d62321d4db722ef4cc5ac1c
Gregg B <514026+greggb@users.noreply.github.com>
2020-02-04 12:23:33 -0500

    move components from snooty and cleanup (#2)
    * move components from snooty and cleanup
    
    * remove docs-tools
    
    * remove docs-tools configs

---
+++ src/pages/index.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Layout from '../components/dev-hub/layout';
+import { H1, H2, P } from '../components/dev-hub/text';
+import { colorMap, size } from '../components/dev-hub/theme';
+
+const LiveStream = styled('div')`
+    background: ${colorMap.greyDarkTwo};
+    border-radius: ${size.large};
+    padding: ${size.default};
+    text-align: center;
+`;
+
+const Hero = styled('header')`
+    color: ${colorMap.devWhite};
+    min-height: 40vh;
+    padding: ${size.large} 0;
+    text-align: center;
+`;
+
+const ArticleGallery = styled('section')`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: ${size.large} 0;
+`;
+
+const ArticleGalleryCards = styled('div')`
+    display: flex;
+    justify-content: flex-start;
+    overflow-x: auto;
+    margin-bottom: ${size.default};
+    width: 100%;
+`;
+
+const ArticleGalleryCard = styled('a')`
+    background: ${colorMap.devWhite};
+    flex: 0 0 288px;
+    height: 400px;
+    margin-right: ${size.large};
+`;
+
+const FeatureSection = styled('section')`
+    display: flex;
+    justify-content: space-between;
+    padding: ${size.large} 0;
+    header {
+        flex: 1;
+    }
+`;
+
+const FeatureSectionCard = styled('a')`
+    background: ${colorMap.devWhite};
+    display: block;
+    height: 408px;
+    width: 368px;
+`;
+
+const FeatureSectionVideo = styled('a')`
+    background: ${colorMap.devWhite};
+    display: block;
+    height: 408px;
+    width: 65%;
+`;
+
+export default ({ ...data }) => {
+    console.log(data);
+    return (
+        <Layout>
+            <LiveStream>Live now</LiveStream>
+            <Hero>
+                <H1>sample code</H1>
+                <P>description text</P>
+                <div>
+                    <button>primary link</button>
+                    <button>secondary link</button>
+                </div>
+            </Hero>
+            <ArticleGallery>
+                <ArticleGalleryCards>
+                    <ArticleGalleryCard>Card</ArticleGalleryCard>
+                    <ArticleGalleryCard>Card</ArticleGalleryCard>
+                    <ArticleGalleryCard>Card</ArticleGalleryCard>
+                    <ArticleGalleryCard>Card</ArticleGalleryCard>
+                    <ArticleGalleryCard>Card</ArticleGalleryCard>
+                    <ArticleGalleryCard>Card</ArticleGalleryCard>
+                </ArticleGalleryCards>
+                <button>secondary link</button>
+            </ArticleGallery>
+            <FeatureSection>
+                <header>
+                    <H2>Developer Inspo</H2>
+                    <P>description text</P>
+                    <div>
+                        <button>primary link</button>
+                        <button>tertiary link</button>
+                    </div>
+                </header>
+                <FeatureSectionCard>Card</FeatureSectionCard>
+            </FeatureSection>
+            <FeatureSection>
+                <FeatureSectionVideo>Video</FeatureSectionVideo>
+                <header>
+                    <H2>Let's do it live</H2>
+                    <P>description text</P>
+                    <div>
+                        <button>primary link</button>
+                    </div>
+                </header>
+            </FeatureSection>
+            <FeatureSection>
+                <header>
+                    <H2>Get Involved</H2>
+                    <P>description text</P>
+                    <div>
+                        <button>primary link</button>
+                        <button>tertiary link</button>
+                    </div>
+                </header>
+                <FeatureSectionCard>Card</FeatureSectionCard>
+            </FeatureSection>
+        </Layout>
+    );
+};
---
+++ src/pages/storybook.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Badge from '../components/dev-hub/badge';
+import { StorybookLayout } from '../components/dev-hub/layout';
+import Link from '../components/dev-hub/link';
+import { H1, H2, H3, H4, P } from '../components/dev-hub/text';
+import { colorMap, size } from '../components/dev-hub/theme';
+
+const StorybookContainer = styled('div')`
+    padding: ${size.default};
+`;
+
+const Swatch = styled('div')`
+    background-color: ${props => props.colorValue};
+    font-weight: bold;
+    padding: ${size.small};
+    margin: ${size.tiny};
+`;
+
+const SectionHeader = styled(H2)`
+    text-decoration: underline;
+`;
+
+export default () => (
+    <StorybookLayout>
+        <StorybookContainer>
+            <H1>DevHub Component "Storybook"</H1>
+            <SectionHeader>Text</SectionHeader>
+            <H1 collapse>Heading 1</H1>
+            <H2>Heading 2</H2>
+            <H3>Heading 3</H3>
+            <H4>Heading 4</H4>
+            <P>Paragraph</P>
+            <SectionHeader>Content Label</SectionHeader>
+            <Badge>How-To</Badge>
+            <Badge>Quick Start</Badge>
+            <Badge>Article</Badge>
+            <Badge>Event</Badge>
+            <Badge>Community</Badge>
+            <Badge>Deep Dive</Badge>
+            <SectionHeader>Links</SectionHeader>
+            <Link primary>Hello World</Link>
+            <SectionHeader>Colors</SectionHeader>
+            {Object.keys(colorMap).map(colorName => (
+                <Swatch
+                    key={colorName}
+                    colorName={colorName}
+                    colorValue={colorMap[colorName]}
+                >
+                    <P collapse>
+                        {colorMap[colorName]} - {colorName}
+                    </P>
+                </Swatch>
+            ))}
+        </StorybookContainer>
+    </StorybookLayout>
+);
