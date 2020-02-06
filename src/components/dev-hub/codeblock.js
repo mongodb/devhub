81529b2b218680c8e5e93dc81c8c543a90259456
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-02-06 16:13:25 -0500

    Add codeblock component

---
+++ src/components/dev-hub/codeblock.js
@@ -0,0 +1,98 @@
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import { Pre } from './text';
+import { size, colorMap } from './theme';
+
+const wrapSingle = css`
+    padding-bottom: ${size.default};
+    overflow-x: scroll;
+    white-space: pre;
+    word-break: keep-all;
+    word-wrap: normal;
+`;
+
+const CodeBlockContainer = styled('div')`
+    display: flex;
+    width: 100%;
+`;
+
+const CodeBlockContent = styled(Pre)`
+    border: none;
+    flex: 1;
+    width: calc(100% - ${size.default} - ${size.default});
+`;
+
+const CodeBlockLine = styled(Pre)`
+    background-color: ${colorMap.greyDarkTwo};
+    border: none;
+`;
+
+const CodeContainer = styled('div')`
+    ${({ singleLine }) => singleLine && wrapSingle};
+`;
+
+const countLineBreaks = children => {
+    let lineCount = 0;
+    for (let i = 0; i < children.length; i++) {
+        if (lineCount > 1) return lineCount;
+        if (typeof children[i] === 'string') {
+            const newLineMatch = children[i].match(/\r?\n|\r/g);
+            if (newLineMatch && newLineMatch.length > 0) {
+                lineCount += newLineMatch.length;
+            }
+        } else {
+            lineCount += countLineBreaks(children[i]);
+        }
+    }
+    return lineCount;
+};
+
+const isSingleLine = children => {
+    return countLineBreaks(children) <= 1;
+};
+
+const CodeBlockLines = ({ content }) => {
+    const numberOfLines = countLineBreaks(content);
+    const lines = useMemo(() => {
+        const result = [];
+        for (let lineNumber = 1; lineNumber < numberOfLines + 1; lineNumber++) {
+            result.push(
+                <span key={lineNumber}>
+                    {lineNumber !== 1 ? <br /> : null}
+                    {lineNumber}
+                </span>
+            );
+        }
+        return result;
+    }, [numberOfLines]);
+    return <CodeBlockLine>{lines}</CodeBlockLine>;
+};
+
+const CodeBlock = ({ children }) => {
+    const singleLine = isSingleLine(children);
+
+    return (
+        <CodeBlockContainer>
+            {!singleLine && <CodeBlockLines content={children} />}
+            <CodeBlockContent>
+                <CodeContainer singleLine={singleLine}>
+                    {children}
+                </CodeContainer>
+                {/* TODO add copy functionality */}
+                {/* <div css={!singleLine && multiLineButton}>
+                        <CopyButton single={singleLine} copyContent={children} />
+                    </div> */}
+            </CodeBlockContent>
+        </CodeBlockContainer>
+    );
+};
+
+CodeBlock.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+CodeBlockContainer.displayName = 'CodeBlock';
+
+export default CodeBlock;
