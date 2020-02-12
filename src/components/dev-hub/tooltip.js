3918e245ab3d5c099089a05da4ca2efdab70483f
Jensen Bouzi <jensen.bouzi@gmail.com>
2020-02-11 22:01:31 -0500

    Replace built in arrow with custom arrow to ensure arrows with borders

--- src/components/dev-hub/tooltip.js
+++ src/components/dev-hub/tooltip.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled-base';
 import { css } from '@emotion/core';
-import Popover, { ArrowContainer } from 'react-tiny-popover';
+import Popover from 'react-tiny-popover';
 import { colorMap, layer, size } from './theme';
 
 const CONTENT_MAX_WIDTH = 250;
 const ARROW_SIZE = 80;
-const TOOLTIP_DISTANCE = 10;
+const TOOLTIP_DISTANCE = 15;
 const gradient = css`
     border-image: linear-gradient(
         315deg,
@@ -19,6 +19,107 @@ const gradient = css`
 const defaultBorder = css`
     border-color: ${colorMap.greyLightTwo};
 `;
+const arrowBase = css`
+    &:before,
+    &:after {
+        border: solid transparent;
+        content: ' ';
+        height: 0;
+        width: 0;
+        position: absolute;
+        pointer-events: none;
+    }
+    &:before {
+        border-color: rgba(159, 237, 208, 0);
+        border-width: 12px;
+        margin-top: -12px;
+    }
+    &:after {
+        border-color: rgba(213, 111, 96, 0);
+        border-width: 9px;
+        margin-top: -9px;
+    }
+`;
+
+const verticalArrowBase = css`
+    &:before,
+    &:after {
+        right: 53%;
+    }
+    &:after {
+        left: calc(50% - 11px);
+        border-width: 11px;
+        margin-top: -11px;
+    }
+    &:before {
+        left: calc(50% - 15px);
+        border-width: 15px;
+        margin-top: -15px;
+    }
+`;
+
+const horizontalArrowBase = css`
+    &:before,
+    &:after {
+        top: 30px;
+    }
+`;
+
+const rightArrowStyles = css`
+    ${arrowBase}
+    ${horizontalArrowBase}
+    &:before,
+    &:after {
+        left: 100%;
+    }
+    &:before {
+        border-left-color: ${colorMap.magenta};
+    }
+    &:after {
+        border-left-color: ${colorMap.greyDarkOne};
+    }
+`;
+const leftArrowStyles = css`
+    ${arrowBase}
+    ${horizontalArrowBase}
+    &:before,
+    &:after {
+        right: 100%;
+    }
+    &:before {
+        border-right-color: ${colorMap.orange};
+    }
+    &:after {
+        border-right-color: ${colorMap.greyDarkOne};
+    }
+`;
+
+const bottomArrowStyles = css`
+    ${arrowBase}
+    ${verticalArrowBase}
+    &:after {
+        border-top-color: ${colorMap.greyDarkOne};
+        top: calc(100% + 11px);
+    }
+    &:before {
+        border-top-color: ${colorMap.magenta};
+        top: calc(100% + 15px);
+    }
+`;
+const topArrowStyles = css`
+    ${arrowBase}
+    ${verticalArrowBase}
+    &:before,
+    &:after {
+        bottom: 100%;
+    }
+    &:after {
+        border-bottom-color: ${colorMap.greyDarkOne};
+    }
+    &:before {
+        border-bottom-color: ${colorMap.magenta};
+    }
+`;
 
 const Trigger = styled('span')`
     cursor: pointer;
@@ -30,8 +131,13 @@ const Content = styled('div')`
     color: ${colorMap.devWhite};
     max-width: ${CONTENT_MAX_WIDTH};
     padding: ${size.medium} ${size.default};
+    position: relative;
     ${({ hasGradientBorder }) => hasGradientBorder && gradient}
     ${({ hasGradientBorder }) => !hasGradientBorder && defaultBorder}
+    ${({ isBottom }) => isBottom && topArrowStyles}
+    ${({ isLeft }) => isLeft && rightArrowStyles}
+    ${({ isRight }) => isRight && leftArrowStyles}
+    ${({ isTop }) => isTop && bottomArrowStyles}
 `;
 
 // This logic was pulled from 'react-tiny-popover' (see = https://github.com/alexkatz/react-tiny-popover/blob/3928cfa67a57676f32d5f04b3e1decb8c31db544/src/Popover.tsx#L316-L352)
@@ -83,22 +189,6 @@ const contentLocation = padding => ({
 
     return { top: finalTop, left: finalLeft };
 };
-/*
-    Default Styling & Positioning for Arrow
-    Based on Tooltip Position (right/left/top/bottom)
-*/
-const ARROW_RECT = {
-    height: ARROW_SIZE,
-    width: ARROW_SIZE,
-};
-
-const ARROW_STYLE_MAP = {
-    bottom: { left: '46%', right: '53%', top: '2px' },
-    left: { right: '2px', top: size.medium },
-    right: { left: '2px', top: size.medium },
-    top: { bottom: '2px', left: '47%', right: '53%' },
-};
-
 // Default alignment of tooltip content based on tooltip position
 const TOOLTIP_ALIGNMENT_MAP = {
     bottom: 'center',
@@ -119,7 +209,7 @@ const Tooltip = ({ children, hasGradientBorder, position, trigger }) => {
     const [isOpen, setIsOpen] = useState(false);
     const tooltipProps = {
         align: TOOLTIP_ALIGNMENT_MAP[position],
-        containerStyle: { zIndex: layer.superFront },
+        containerStyle: { overflow: 'visible', zIndex: layer.superFront },
         disableReposition: true,
         isOpen: isOpen,
         onClickOutside: () => setIsOpen(false),
@@ -132,18 +222,15 @@ const Tooltip = ({ children, hasGradientBorder, position, trigger }) => {
         <Popover
             contentLocation={contentLocation(TOOLTIP_DISTANCE)}
             content={
-                <ArrowContainer
-                    arrowColor={colorMap.greyDarkOne}
-                    arrowSize={10}
-                    arrowStyle={ARROW_STYLE_MAP[position]}
-                    position={position}
-                    popoverRect={ARROW_RECT}
-                    targetRect={ARROW_RECT}
+                <Content
+                    isTop={position === 'top'}
+                    isBottom={position === 'bottom'}
+                    isRight={position === 'right'}
+                    isLeft={position === 'left'}
+                    hasGradientBorder={hasGradientBorder}
                 >
-                    <Content hasGradientBorder={hasGradientBorder}>
-                        {children}
-                    </Content>
-                </ArrowContainer>
+                    {children}
+                </Content>
             }
             {...tooltipProps}
         >
