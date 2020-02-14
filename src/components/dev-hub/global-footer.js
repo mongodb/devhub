a22569a9a7ad99aa8406be6c8aed8dc9fa86d8f3
melissarudofsky <melissa.rudofsky@mongodb.com>
2020-02-14 12:43:17 -0500

    add footer component

---
+++ src/components/dev-hub/global-footer.js
@@ -0,0 +1,168 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+import { colorMap, fontSize, lineHeight, screenSize, size } from './theme';
+import MongodbLogoIcon from './icons/mongodb-logo';
+import Link from './link';
+import FacebookIcon from './icons/facebook-icon';
+import TwitterIcon from './icons/twitter-icon';
+import LinkedIn from './icons/linkedin';
+import Github from './icons/github';
+import Youtube from './icons/youtube';
+import Twitch from './icons/twitch';
+
+const siteLinks = [
+    {
+        name: 'Developer Hub',
+        url: '/',
+    },
+    {
+        name: 'Documentation',
+        url: 'https://docs.mongodb.com/',
+    },
+    {
+        name: 'University',
+        url: 'https://university.mongodb.com/',
+    },
+    {
+        name: 'Community',
+        url: 'https://community.mongodb.com/',
+    },
+];
+const iconProps = {
+    height: 15,
+    width: 15,
+    color: colorMap.greyLightTwo,
+};
+const iconLinks = [
+    {
+        name: <Github {...iconProps} />,
+        url: 'https://github.com/mongodb',
+    },
+    {
+        name: <Twitch {...iconProps} />,
+        url: 'https://m.twitch.tv/mongodb/profile',
+    },
+    {
+        name: <Youtube {...iconProps} />,
+        url: 'https://www.youtube.com/user/mongodb',
+    },
+    {
+        name: <TwitterIcon {...iconProps} />,
+        url: 'https://twitter.com/MongoDB/',
+    },
+    {
+        name: <FacebookIcon {...iconProps} />,
+        url: 'https://www.facebook.com/MongoDB/',
+    },
+    {
+        name: <LinkedIn {...iconProps} />,
+        url: 'https://www.linkedin.com/company/mongodbinc/',
+    },
+];
+const GlobalFooter = styled('footer')`
+    background: ${colorMap.greyDarkThree};
+    color: ${colorMap.greyLightTwo};
+    display: grid;
+    @media ${screenSize.mediumAndUp} {
+        grid-gap: 0 ${size.large};
+        grid-template-areas:
+            'logo list'
+            'logo icons'
+            'logo copyright';
+        grid-template-columns: 25% 70%;
+        padding: ${size.large};
+    }
+    @media ${screenSize.upToMedium} {
+        grid-template-areas:
+            'list list'
+            'icons icons'
+            'logo copyright';
+        grid-template-columns: 49% 49%;
+        padding: 0 ${size.default};
+    }
+`;
+const logoStyles = css`
+    @media ${screenSize.upToMedium} {
+        height: 26px;
+        width: 100px;
+    }
+`;
+const LogoContainer = styled('section')`
+    grid-area: logo;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    @media ${screenSize.mediumAndUp} {
+        border-right: 1px solid ${colorMap.greyLightTwo};
+        padding: ${size.large};
+    }
+    @media ${screenSize.upToMedium} {
+        border-top: 1px solid ${colorMap.greyLightTwo};
+        padding: ${size.default} 0;
+    }
+`;
+const listStyles = css`
+    display: flex;
+    list-style-type: none;
+    padding: 0;
+    @media ${screenSize.upToMedium} {
+        justify-content: center;
+    }
+`;
+const LinksList = styled('ul')`
+    ${listStyles}
+    grid-area: list;
+    @media ${screenSize.upToSmall} {
+        flex-direction: column;
+    }
+`;
+const IconList = styled('ul')`
+    ${listStyles}
+    @media ${screenSize.upToSmall} {
+        justify-content: space-between;
+    }
+    grid-area: icons;
+`;
+const Copyright = styled('p')`
+    font-size: ${fontSize.micro};
+    grid-area: copyright;
+    @media ${screenSize.upToMedium} {
+        align-items: center;
+        border-top: 1px solid ${colorMap.greyLightTwo};
+        display: flex;
+        justify-content: center;
+        margin: 0;
+        padding: ${size.default} 0;
+    }
+`;
+const FooterLink = styled(Link)`
+    color: ${colorMap.greyLightTwo};
+    font-size: ${fontSize.small};
+    line-height: ${lineHeight.small};
+    text-decoration: none;
+`;
+const ListItem = styled('li')`
+    @media ${screenSize.smallAndUp} {
+        &:not(:first-of-type) {
+            margin-left: ${size.large};
+        }
+    }
+`;
+const getLinksList = link => (
+    <ListItem key={link.url}>
+        <FooterLink href={link.url}>{link.name}</FooterLink>
+    </ListItem>
+);
+export default () => {
+    return (
+        <GlobalFooter>
+            <LogoContainer>
+                <MongodbLogoIcon css={logoStyles} />
+            </LogoContainer>
+            <LinksList>{siteLinks.map(getLinksList)}</LinksList>
+            <IconList>{iconLinks.map(getLinksList)}</IconList>
+            <Copyright>© {new Date().getFullYear()} MongoDB, Inc.</Copyright>
+        </GlobalFooter>
+    );
+};
