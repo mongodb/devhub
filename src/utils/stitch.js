7e1f96d101d4539ffb8e90ee7cd8c28423219e04
sophstad <sophie.stadler@mongodb.com>
2020-02-18 16:35:54 -0500

    Add function description and fix array spread

--- src/utils/stitch.js
+++ src/utils/stitch.js
@@ -8,9 +8,21 @@ const initializeApp = appId =>
 
 const stitchClient = isBrowser() ? initializeApp('snooty-koueq') : {};
 
+/**
+ * Calls a function from Stitch and returns array of documents that match the specified query filters.
+ * @param {string} fnName
+ * @param {Object<string, any>} metadata (see hooks/use-site-metadata.js)
+ * @property {string} commitHash
+ * @property {string} database
+ * @property {string} parserBranch
+ * @property {string} project
+ * @property {string} user
+ * @param {Array<any>} fnArgs
+ * @returns {array} array of MongoDB documents
+ */
 export const callStitchFunction = async (fnName, metadata, fnArgs) => {
     try {
-        return stitchClient.callFunction(fnName, [metadata, fnArgs]);
+        return stitchClient.callFunction(fnName, [metadata, ...fnArgs]);
     } catch (error) {
         console.error(error);
     }
