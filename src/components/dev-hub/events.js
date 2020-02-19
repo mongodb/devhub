10462b4b4d54be1c16940381a6891d836bbb5622
Jensen Bouzi <jensen.bouzi@gmail.com>
2020-02-19 16:40:30 -0500

    Create components for events section of community page

---
+++ src/components/dev-hub/events.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+import LocationIcon from './icons/location-icon';
+import MediaBlock from './media-block';
+import GradientImage from './gradient-image';
+import Link from './link';
+import { H1, H2, P, H4 } from './text';
+import {
+    gradientMap,
+    size,
+    colorMap,
+    fontSize,
+    animationSpeed,
+    screenSize,
+} from './theme';
+
+const sampleEvents = [
+    {
+        date: new Date('January 20, 2020'),
+        title: 'MongoDB.local San Francisco',
+        location: 'San Francisco, California',
+        url: '/community',
+    },
+    {
+        date: new Date('January 29, 2020'),
+        title: 'Coffe With Your Data: Real-Time Analytics',
+        location: 'New York, New York',
+        url: '/community',
+    },
+    {
+        date: new Date('February 13, 2020'),
+        title: 'Happy Hour & Arcade',
+        location: 'Los Angeles, California',
+        url: '/community',
+    },
+];
+
+const Day = styled('span')`
+    display: block;
+    font-size: ${fontSize.medium};
+    font-weight: bold;
+    margin-bottom: 0;
+`;
+const Month = styled('span')`
+    display: block;
+    font-size: ${fontSize.xsmall};
+    margin-top: -${size.small};
+`;
+
+const CalendarDate = styled('div')`
+    background-color: ${colorMap.greyDarkThree};
+    border-radius: ${size.xsmall};
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: center;
+    text-align: center;
+`;
+
+const StyledDate = styled('div')`
+    background: ${gradientMap.greenTeal};
+    border-radius: ${size.xsmall};
+    grid-area: image;
+    height: ${size.xlarge};
+    margin-right: ${size.medium};
+    padding: 2px;
+    width: ${size.xlarge};
+`;
+
+const EventInfo = styled('div')`
+    grid-area: content;
+`;
+
+const Location = styled(P)`
+    color: ${colorMap.greyLightThree};
+    font-size: ${fontSize.xsmall};
+    margin: 0;
+`;
+
+const Title = styled(H4)`
+    margin: 0;
+`;
+
+const StyledEvent = styled(Link)`
+    border-radius: ${size.xsmall};
+    cursor: pointer;
+    display: grid;
+    flex-direction: row;
+    grid-template-areas: 'image content';
+    grid-template-columns: 1fr 3fr;
+    margin-right: ${size.medium};
+    max-width: 400px;
+    padding: ${size.medium};
+    text-decoration: none;
+
+    &:focus,
+    &:hover {
+        color: ${colorMap.devWhite};
+        background: ${colorMap.greyDarkTwo};
+        div[data-name='event-date'] {
+            background: ${gradientMap.greenTeal};
+        }
+        p[data-name='event-location'] {
+            color: ${colorMap.devWhite};
+        }
+    }
+`;
+
+const AllEvents = styled('div')`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    @media ${screenSize.upToMedium} {
+        flex-direction: column;
+        justify-content: start;
+    }
+`;
+
+const DateIcon = ({ date }) => {
+    const day = date.getUTCDate();
+    const month = date.toLocaleString('default', {
+        month: 'short',
+        timezone: 'UTC',
+    });
+    return (
+        <StyledDate>
+            <CalendarDate data-name="event-date">
+                <Day>{day}</Day>
+                <Month>{month}</Month>
+            </CalendarDate>
+        </StyledDate>
+    );
+};
+
+const Event = ({ date, location, title, url }) => (
+    <StyledEvent to={url}>
+        <DateIcon date={date} />
+        <EventInfo>
+            <Title>{title}</Title>
+            <Location data-name="event-location">
+                <LocationIcon
+                    color={colorMap.greyLightThree}
+                    height="15px"
+                    width="15px"
+                />{' '}
+                {location}
+            </Location>
+        </EventInfo>
+    </StyledEvent>
+);
+
+const EventList = ({ events = sampleEvents }) => (
+    <AllEvents>
+        {events.map(event => (
+            <Event key={event.title} {...event} />
+        ))}
+    </AllEvents>
+);
+export { Event, EventList };
