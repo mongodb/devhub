fc8e2a930601cb0109b5b579b656b04f68bed077
melissarudofsky <melissa.rudofsky@mongodb.com>
2020-02-27 14:53:21 -0500

    submit the project form

--- src/components/dev-hub/project-sign-up-form.js
+++ src/components/dev-hub/project-sign-up-form.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import Modal from './modal';
 import styled from '@emotion/styled';
-import { size } from './theme';
-import { H5 } from './text';
+import { colorMap, size } from './theme';
+import { H3, H5, P } from './text';
 import Input from './input';
 import Button from './button';
 import TextArea from './text-area';
+import { useSiteMetadata } from '../../hooks/use-site-metadata';
+import { authenticate, callStitchFunction } from '../../utils/stitch';
+import SuccessIcon from './icons/success';
 
 const ModalContainer = styled('div')`
     padding: 0 ${size.default};
@@ -22,20 +25,49 @@ const Title = styled(H5)`
 const StyledInput = styled(Input)`
     margin-bottom: ${size.large};
 `;
-const Form = React.memo(() => {
+const SuccessContainer = styled('div')`
+    text-align: center;
+    svg {
+        margin-bottom: ${size.large};
+    }
+`;
+const ErrorMessage = styled(P)`
+    color: ${colorMap.salmon};
+`;
+const callStitch = async (metadata, object, callback) => {
+    try {
+        const res = await callStitchFunction(
+            'submitDevhubProject',
+            metadata,
+            object
+        );
+        res && callback(true);
+    } catch {
+        callback(false);
+    }
+};
+
+const Form = React.memo(({ setSuccess, success }) => {
+    const metadata = useSiteMetadata();
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [project, setProject] = useState('');
+    const [projectDescription, setProjectDescription] = useState('');
     const handleSubmit = e => {
         e.preventDefault();
-        console.log({
+        const obj = {
             name,
             email,
-            project,
-        });
+            projectDescription,
+        };
+        authenticate();
+        callStitch(metadata, obj, setSuccess);
     };
     return (
         <form onSubmit={handleSubmit}>
+            <ErrorMessage>
+                {success === false &&
+                    'Your submission failed. Please try again.'}
+            </ErrorMessage>
             <StyledInput
                 value={name}
                 required
@@ -49,10 +81,10 @@ const Form = React.memo(() => {
                 onChange={e => setEmail(e.target.value)}
             />
             <TextArea
-                value={project}
+                value={projectDescription}
                 required
                 placeholder="Project Description"
-                onChange={e => setProject(e.target.value)}
+                onChange={e => setProjectDescription(e.target.value)}
             />
             <SubmitContainer>
                 <Button type="submit" primary>
@@ -63,21 +95,38 @@ const Form = React.memo(() => {
     );
 });
 
-export default ({ triggerComponent }) => {
+const SuccessState = () => (
+    <SuccessContainer>
+        <SuccessIcon />
+        <H3>Thank you for sharing!</H3>
+    </SuccessContainer>
+);
+
+const ModalContent = () => {
+    const [success, setSuccess] = useState(null);
+    if (success) {
+        return <SuccessState />;
+    }
     return (
-        <Modal
-            dialogContainerStyle={{
-                width: '800px',
-            }}
-            dialogMobileContainerStyle={{
-                width: '100%',
-            }}
-            triggerComponent={triggerComponent}
-        >
-            <ModalContainer>
-                <Title>Submit Your Project</Title>
-                <Form />
-            </ModalContainer>
-        </Modal>
+        <>
+            <Title>Submit Your Project</Title>
+            <Form setSuccess={setSuccess} success={success} />
+        </>
     );
 };
+
+export default ({ triggerComponent }) => (
+    <Modal
+        dialogContainerStyle={{
+            width: '800px',
+        }}
+        dialogMobileContainerStyle={{
+            width: '100%',
+        }}
+        triggerComponent={triggerComponent}
+    >
+        <ModalContainer>
+            <ModalContent />
+        </ModalContainer>
+    </Modal>
+);
