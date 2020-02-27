3bcf50bef9794145124c4eaab40ea07d72177856
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-02-27 16:44:44 -0500

    Map to pageList, switch metadata name

--- gatsby-node.js
+++ gatsby-node.js
@@ -123,12 +123,11 @@ const createTagPageType = async (createPage, pageMetadata, stitchType) => {
         pages: pageData[i],
     }));
 
-    const PAGES = [];
-
-    itemsWithPageData.forEach(page => {
+    const pageList = itemsWithPageData.map(page => {
         const name = isAuthor ? page.item._id.name : page.item._id;
         // Some bad data for authors doesn't follow this structure, so ignore it
-        if (name) {
+        if (!name) return null;
+        else {
             const urlSuffix = isAuthor
                 ? encodeURIComponent(
                       name
@@ -146,21 +145,23 @@ const createTagPageType = async (createPage, pageMetadata, stitchType) => {
             if (isAuthor) {
                 newPage['author_image'] = page.item._id.image;
             }
-            PAGES.push(newPage);
+            return newPage;
         }
     });
 
-    PAGES.forEach(page => {
-        console.log(page.slug);
-        createPage({
-            path: page.slug,
-            component: path.resolve(`./src/templates/tag.js`),
-            context: {
-                metadata: pageMetadata,
-                snootyStitchId: SNOOTY_STITCH_ID,
-                ...page,
-            },
-        });
+    pageList.forEach(page => {
+        if (page) {
+            console.log(page.slug);
+            createPage({
+                path: page.slug,
+                component: path.resolve(`./src/templates/tag.js`),
+                context: {
+                    metadata: pageMetadata,
+                    snootyStitchId: SNOOTY_STITCH_ID,
+                    ...page,
+                },
+            });
+        }
     });
 };
 
@@ -214,7 +215,7 @@ exports.sourceNodes = async () => {
 
 exports.createPages = async ({ actions }) => {
     const { createPage } = actions;
-    const pageMetadata = await stitchClient.callFunction('fetchDocument', [
+    const metadata = await stitchClient.callFunction('fetchDocument', [
         DB,
         METADATA_COLLECTION,
         constructDbFilter(),
@@ -232,7 +233,7 @@ exports.createPages = async ({ actions }) => {
                 path: slug,
                 component: path.resolve(`./src/templates/${template}.js`),
                 context: {
-                    metadata: pageMetadata,
+                    metadata: metadata,
                     slug,
                     snootyStitchId: SNOOTY_STITCH_ID,
                     __refDocMapping: pageNodes,
@@ -242,11 +243,11 @@ exports.createPages = async ({ actions }) => {
     });
 
     await Promise.all([
-        createTagPageType(createPage, pageMetadata, 'author'),
-        createTagPageType(createPage, pageMetadata, 'languages'),
-        createTagPageType(createPage, pageMetadata, 'products'),
-        createTagPageType(createPage, pageMetadata, 'tags'),
-        createTagPageType(createPage, pageMetadata, 'type'),
+        createTagPageType(createPage, metadata, 'author'),
+        createTagPageType(createPage, metadata, 'languages'),
+        createTagPageType(createPage, metadata, 'products'),
+        createTagPageType(createPage, metadata, 'tags'),
+        createTagPageType(createPage, metadata, 'type'),
     ]);
 };
 
