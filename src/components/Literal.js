b83907dc94e0ddaa3b8870013a3195b5ec480884
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-02-28 15:15:29 -0500

    Add overflow wrap to literal values

--- src/components/Literal.js
+++ src/components/Literal.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { css } from '@emotion/core';
 import PropTypes from 'prop-types';
 import { getNestedValue } from '../utils/get-nested-value';
 
+const breakWordOverflow = css`
+    overflow-wrap: break-word;
+`;
+
 const Literal = ({ nodeData }) => (
     <code className="docutils literal notranslate">
-        <span className="pre">
+        <span className="pre" css={breakWordOverflow}>
             {getNestedValue(['children', 0, 'value'], nodeData)}
         </span>
     </code>
