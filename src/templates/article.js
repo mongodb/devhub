d56b78170eda056af08d2f259e28a13bf33701ff
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-02-28 10:37:36 -0500

    Remove verify date field comment

--- src/templates/article.js
+++ src/templates/article.js
@@ -134,7 +134,6 @@ const Article = props => {
                 originalDate={meta.pubdate}
                 tags={tagList}
                 title={articleTitle}
-                // TODO: Verify field name with Sophie
                 updatedDate={meta.updatedDate}
             />
 
