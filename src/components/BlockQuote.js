3cfe56f6ebbb45d8334a6d188bfcdf9c255b2b7d
Gregg Brewster <mail@digitalpuddle.com>
2020-02-29 12:58:36 -0500

    clear blockquote child bottom margin

--- src/components/BlockQuote.js
+++ src/components/BlockQuote.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { css } from '@emotion/core';
 import PropTypes from 'prop-types';
 import ComponentFactory from './ComponentFactory';
 
+const clearBottom = css`
+    p:last-of-type {
+        margin-bottom: 0;
+    }
+`;
+
 const BlockQuote = ({ nodeData: { children }, ...rest }) => (
-    <blockquote>
+    <blockquote css={clearBottom}>
         {children.map((element, index) => (
             <ComponentFactory {...rest} nodeData={element} key={index} />
         ))}
