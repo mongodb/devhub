1c3b9716f39e9734550c0232cc0667a84faee52e
Gregg B <514026+greggb@users.noreply.github.com>
2020-03-03 15:02:38 -0500

    add sitemap plugin (#164)

--- gatsby-config.prod.js
+++ gatsby-config.prod.js
@@ -11,6 +11,7 @@ module.exports = {
     plugins: [
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-emotion',
+        'gatsby-plugin-sitemap',
         {
             resolve: 'gatsby-plugin-google-tagmanager',
             options: {
