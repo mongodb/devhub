fffab0f7bf921998a7b5db29efd8a762c8b3be7b
Jensen Bouzi <jensen.bouzi@gmail.com>
2020-03-04 09:57:46 -0500

    Add missing nodeJs illustration and update images on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,8 @@ import greenPatternImage from '../images/2x/pattern-green@2x.png';
 import productivityImage from '../images/2x/Productivity@2x.png';
 import meetupsImage from '../images/1x/Meetups.png';
 import unityImage from '../images/2x/Unity@2x.png';
-import pythonImage from '../images/2x/Charts@2x.png';
-import chartsImage from '../images/1x/Python.png';
+import nodejsIllustration from '../images/1x/Node.Js-Illustration.png';
+import pythonImage from '../images/1x/Python.png';
 import GradientUnderline from '../components/dev-hub/gradient-underline';
 import homepageBackground from '../images/1x/homepage-background.png';
 import ProjectSignUpForm from '../components/dev-hub/project-sign-up-form';
@@ -168,7 +168,7 @@ export default () => {
                     <Sub>What will you create today?</Sub>
                     <CardGallery>
                         <StyledTopCard
-                            image={chartsImage}
+                            image={nodejsIllustration}
                             to="/how-to/nextjs-building-modern-applications"
                         >
                             Building Modern Applications with Next.js and
