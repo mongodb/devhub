66c593c2e78aeaf25d3e59742cef45854d751cad
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-03-05 15:42:21 -0500

    Add config options for docs

--- .storybook/config.js
+++ .storybook/config.js
@@ -8,6 +8,12 @@ addParameters({
         page: DocsPage,
         prepareForInline: storyFn => storyFn(),
     },
+    options: {
+        storySort: (a, b) =>
+            a[1].kind === b[1].kind
+                ? 0
+                : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+    },
 });
 
 // Gatsby's Link overrides:
