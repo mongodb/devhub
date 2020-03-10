112e82207cd861ff4d6d76ca37a80a95237cfa17
Jensen Bouzi <jensen.bouzi@gmail.com>
2020-03-10 10:50:59 -0400

    WIP setting up theme

---
+++ .storybook/theme.js
@@ -0,0 +1,38 @@
+import { create } from '@storybook/theming/create';
+import { colorMap, FORM_ELEMENT_BORDER } from '../src/components/dev-hub/theme';
+
+export default create({
+  base: colorMap.pageBackground,
+
+  colorPrimary: 'hotpink',
+  colorSecondary: 'deepskyblue',
+
+  // UI
+  appBg: 'white',
+  appContentBg: 'silver',
+  appBorderColor: 'grey',
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: `akzidenz, -apple-system, BlinkMacSystemFont, "Segoe UI",
+            Helvetica, Arial, sans-serif, 'Apple Color Emoji', "Segoe UI Emoji",
+            "Segoe UI Symbol"`,
+
+  // Text colors
+  textColor: '#fff',
+  textInverseColor: colorMap.devWhite,
+
+  // Toolbar default and active colors
+  barTextColor: '#fff',
+  barSelectedColor: colorMap.devBlack,
+  barBg: colorMap.greyDarkThree,
+
+  // Form colors
+  inputBg: colorMap.greyDarkTwo,
+  inputBorder: `${FORM_ELEMENT_BORDER} solid transparent`,
+  inputTextColor: '#fff',
+  inputBorderRadius: 4,
+
+  brandTitle: 'MongDB Dev-Hub',
+//   brandImage: 'https://placehold.it/350x150',
+});
