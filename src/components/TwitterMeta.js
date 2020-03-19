cd0d809ab3edcf3dbab10ac9ac0204c6dbf8915e
Gregg B <514026+greggb@users.noreply.github.com>
2020-03-19 10:28:51 -0400

    use full image path for meta tags (#243)

--- src/components/TwitterMeta.js
+++ src/components/TwitterMeta.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { getNestedText } from '../utils/get-nested-text';
+import { useSiteMetadata } from '../hooks/use-site-metadata';
 
-export default ({ nodeData: { children, options } }) => (
-    <Helmet>
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content={options.site} />
-        <meta name="twitter:creator" content={options.creator} />
-        <meta property="twitter:title" content={options.title} />
-        <meta
-            property="twitter:description"
-            content={getNestedText(children)}
-        />
-        <meta property="twitter:image" content={options.image} />
-        {/* use twitter image for OG temporarily */}
-        <meta property="og:image" content={options.image} />
-    </Helmet>
-);
+export default ({ nodeData: { children, options } }) => {
+    const { siteUrl } = useSiteMetadata();
+    return (
+        <Helmet>
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content={options.site} />
+            <meta name="twitter:creator" content={options.creator} />
+            <meta property="twitter:title" content={options.title} />
+            <meta
+                property="twitter:description"
+                content={getNestedText(children)}
+            />
+            <meta property="twitter:image" content={siteUrl + options.image} />
+
+            <meta property="og:image" content={siteUrl + options.image} />
+        </Helmet>
+    );
+};
