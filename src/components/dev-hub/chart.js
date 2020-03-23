84923cbd759da158aed0e704224d800a2f90e841
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-03-23 13:14:30 -0400

    Pull out prop types and extra consts, fix autorefresh

--- src/components/dev-hub/chart.js
+++ src/components/dev-hub/chart.js
@@ -1,15 +1,15 @@
 import React, { useMemo } from 'react';
-import PropTypes from 'prop-types';
 import { buildQueryString } from '../../utils/query-string';
 import styled from '@emotion/styled';
 
+const DEFAULT_CHART_AUTOREFRESH = 3600;
 const DEFAULT_CHART_HEIGHT = '570';
 const DEFAULT_CHART_WIDTH = '760';
 const DEFAULT_CHART_THEME = 'dark';
 
 const buildChartUrl = options => {
     const params = {
-        autorefresh: options.autorefresh,
+        autorefresh: options.autorefresh || DEFAULT_CHART_AUTOREFRESH,
         id: options.id,
         theme: options.theme || DEFAULT_CHART_THEME,
     };
@@ -23,35 +23,16 @@ const StyledChart = styled('iframe')`
 `;
 
 const Chart = ({ nodeData: { options } }) => {
-    const chartHeight = options.height || DEFAULT_CHART_HEIGHT;
-    const chartWidth = options.width || DEFAULT_CHART_WIDTH;
-    const chartTitle = options.title;
-    const customAlign = options.align;
     const chartSrc = useMemo(() => buildChartUrl(options), [options]);
     return (
         <StyledChart
-            customAlign={customAlign}
-            height={chartHeight}
-            title={chartTitle}
+            customAlign={options.align}
+            height={options.height || DEFAULT_CHART_HEIGHT}
+            title={options.title}
             src={chartSrc}
-            width={chartWidth}
+            width={options.width || DEFAULT_CHART_WIDTH}
         />
     );
-};
-
-Chart.propTypes = {
-    className: PropTypes.string,
-    nodeData: PropTypes.shape({
-        options: PropTypes.shape({
-            align: PropTypes.string,
-            autorefresh: PropTypes.string,
-            height: PropTypes.string,
-            id: PropTypes.string,
-            theme: PropTypes.string,
-            url: PropTypes.string,
-            width: PropTypes.string,
-        }),
-    }),
 };
 
 export default Chart;
