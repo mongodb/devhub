0e635551d292a1467ebada84cd4bd8fb882787ae
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-04-14 16:10:38 -0400

    Add integration testing for author pages

---
+++ cypress/integration/author.js
@@ -0,0 +1,28 @@
+const AUTHOR_NAME = 'Maxime Beugnet';
+const AUTHOR_URL = '/author/maxime-beugnet';
+
+describe('Author Page', () => {
+    it('should show an image for the author', () => {
+        cy.visit(AUTHOR_URL);
+        cy.get('[data-test="author-image"]').should('exist');
+        cy.get('[data-test="author-image"]').within(() => {
+            cy.get('div')
+                .should('have.css', 'background-image')
+                .should('not.be.empty');
+        });
+    });
+    it('should display a name and description for the author', () => {
+        cy.get('header').within(() => {
+            cy.contains(AUTHOR_NAME);
+            // Description is the only 'p' in the header and content can change
+            cy.contains('p').should('not.be.empty');
+        });
+    });
+    it('should show articles for this author', () => {
+        cy.get('[data-test="card"]')
+            .first()
+            .then(card => {
+                cy.checkArticleCard(card);
+            });
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -18,3 +18,21 @@ Cypress.Commands.add('closeModal', () => {
 Cypress.Commands.add('useBodyReference', () => {
     cy.get('body').as('body');
 });
+
+// Basic sanity checks for an article card (image, title, tags, etc)
+Cypress.Commands.add('checkArticleCard', card => {
+    cy.get(card).within(() => {
+        // Title
+        cy.get('h5').should('not.be.empty');
+        // Description
+        cy.get('p').should('not.be.empty');
+        cy.get('img')
+            .should('have.attr', 'src')
+            .should('not.be.empty');
+        // Tags
+        cy.get('ul li a')
+            .first()
+            .should('have.attr', 'href')
+            .should('match', /\/(tag|product|language)/);
+    });
+});
