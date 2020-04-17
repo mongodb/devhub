724a4b9cd1dd3783b976576f3299a46695765f8b
tahiya-mongo <tahiya.chowdhury@10gen.com>
2020-04-17 11:49:32 -0400

    Removed tester code from media.js

--- src/pages/media.js
+++ src/pages/media.js
@@ -1,25 +1,4 @@
 import React from 'react';
 import Layout from '../components/dev-hub/layout';
-import fetchYoutubeData from '../utils/fetch-youtube-data';
 
-const YT_PLAYLIST_ID = 'PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3';
-
-const getVideos = async () => {
-    const videos = await fetchYoutubeData(YT_PLAYLIST_ID);
-    return videos;
-};
-
-const YoutubeVideo = ({ videoID }) => (
-    <iframe
-        src="http://www.youtube.com/embed/${videoID}"
-        width="560"
-        height="315"
-        frameBorder="0"
-        allowFullScreen
-    ></iframe>
-);
-
-export default () => {
-    const videos = getVideos();
-    return <Layout>{<YoutubeVideo videoID={videos[0]} />}</Layout>;
-};
+export default () => <Layout></Layout>;
