a1792195485feab62c65fe37e7d9f08c44e850e7
Gregg Brewster <mail@digitalpuddle.com>
2020-05-04 13:41:55 -0400

    allow adding static images

--- src/utils/setup/article-rss-feed.js
+++ src/utils/setup/article-rss-feed.js
@@ -1,14 +1,16 @@
 const { rssFeedArticleData } = require('../../queries/rss-feed-article-data');
 const { serializeRssData } = require('./serialize-rss-data');
 
+const siteUrl = 'https://developer.mongodb.com';
+
 const articleRssFeed = {
     serialize: serializeRssData,
     query: rssFeedArticleData,
     output: '/rss.xml',
     title: 'MongoDB Developer Hub',
-    image_url: '/images/MongoDB_Leaf.svg',
-    site_url: 'https://developer.mongodb.com',
-    feed_url: 'https://developer.mongodb.com/rss.xml',
+    image_url: siteUrl + '/images/MongoDB_Leaf.svg',
+    site_url: siteUrl,
+    feed_url: siteUrl + '/rss.xml',
 };
 
 module.exports = { articleRssFeed };
