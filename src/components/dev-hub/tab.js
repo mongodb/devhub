116a27e69505424d6967e5ab3af5d05ba648cf19
tahiya-mongo <tahiya.chowdhury@10gen.com>
2020-05-04 17:20:50 -0400

    Abstracting the tab component

--- src/components/dev-hub/tab.js
+++ src/components/dev-hub/tab.js
@@ -1,116 +1,79 @@
-import { React, useState } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
-import { colorMap, size } from './theme';
-import { P } from './text';
-import CardList from './card-list';
-import useAllVideos from '../../hooks/use-all-videos';
-import usePodcasts from '../../hooks/use-podcasts';
+import { css } from '@emotion/core';
+import { colorMap, size, fontSize } from './theme';
 
 const Tab = styled('div')`
-    overflow: hidden;
+    display: flex;
+    justify-content: space-between;
     border-bottom: 1px solid ${colorMap.greyDarkOne};
-    margin-left: 38px;
-    margin-right: 38px;
-    margin-top: 50px;
-    margin-bottom: 30px;
+`;
+
+const activeStyles = css`
+    color: ${colorMap.devWhite};
+    border-bottom-color: ${colorMap.darkGreen};
+`;
+
+const defaultStyles = css`
+    &:hover {
+        color: ${colorMap.devWhite};
+    }
 `;
 
 const TabButton = styled('button')`
-    float: right;
     border: none;
     outline: none;
     cursor: pointer;
     transition: 0.3s;
-    font-size: 18px;
-    font-family: Fira Mono;
-    color: ${colorMap.greyDarkOne};
     background-color: inherit;
-    border-bottom: 3px solid transparent;
     width: 136px;
     padding-top: ${size.micro};
     padding-bottom: ${size.medium};
     padding-left: ${size.mediumLarge};
     padding-right: ${size.mediumLarge};
-    &:hover {
-        color: white;
-    }
-    &:focus {
-        border-bottom-color: ${colorMap.darkGreen};
-    }
+    border-bottom: 3px solid transparent;
+
+    font-size: ${fontSize.default};
+    color: ${colorMap.greyDarkOne};
+    font-family: 'Fira Mono';
+
+    ${({ isActive }) => (isActive ? activeStyles : defaultStyles)}
 `;
 
-const AllContentStyle = {
-    marginRight: '818px',
-    width: '136px',
-};
+const LeftContent = styled('div')``;
 
-export default () => {
-    const { videos, error, isLoading } = useAllVideos();
-    const {
-        podcasts,
-        error: errorPodcasts,
-        isLoading: isLoadingPodcasts,
-    } = usePodcasts();
+const RightContent = styled('div')``;
 
-    const [showVideos, setShowVideos] = useState(false);
-    const [showPodcasts, setShowPodcasts] = useState(false);
-    const [showArticles, setShowArticles] = useState(false);
-    const [showAll, setShowAll] = useState(true);
-
-    const displayVideos = () => {
-        setShowAll(false);
-        setShowVideos(true);
-        setShowPodcasts(false);
-        setShowArticles(false);
-    };
-
-    const displayPodcasts = () => {
-        setShowAll(false);
-        setShowPodcasts(true);
-        setShowVideos(false);
-        setShowArticles(false);
-    };
-
-    const displayArticles = () => {
-        setShowAll(false);
-        setShowArticles(true);
-        setShowPodcasts(false);
-        setShowVideos(false);
-    };
-
-    const displayAll = () => {
-        setShowAll(true);
-        setShowArticles(false);
-        setShowVideos(false);
-        setShowPodcasts(false);
-    };
-
+export default ({ handleClick, tabTexts, activeItem }) => {
     return (
-        <div>
-            <Tab collapse>
-                <TabButton onClick={() => displayVideos()}>Videos</TabButton>
-                <TabButton onClick={() => displayPodcasts()}>
-                    Podcasts
-                </TabButton>
-                <TabButton onClick={() => displayArticles()}>
-                    Articles
-                </TabButton>
-                <TabButton style={AllContentStyle} onClick={() => displayAll()}>
-                    All
-                </TabButton>
-            </Tab>
+        <Tab>
+            <RightContent>
+                {tabTexts.slice(0, 1).map(item => {
+                    const isActive = item === activeItem ? 'true' : null;
+                    return (
+                        <TabButton
+                            isActive={isActive}
+                            onClick={() => handleClick(item)}
+                        >
+                            {item}
+                        </TabButton>
+                    );
+                })}
+            </RightContent>
 
-            {(isLoading || isLoadingPodcasts) && <P>Loading...</P>}
-
-            {/*TODO: display articles once we have access to them*/}
-
-            {showAll && <CardList videos={videos} podcasts={podcasts} />}
-            {showVideos && <CardList videos={videos} />}
-            {showPodcasts && <CardList podcasts={podcasts} />}
-
-            {(error || errorPodcasts) && (
-                <P>Check back later for upcoming media contents</P>
-            )}
-        </div>
+            <LeftContent>
+                {tabTexts.slice(1).map(item => {
+                    const isActive = item === activeItem ? 'true' : null;
+                    return (
+                        <TabButton
+                            isActive={isActive}
+                            onClick={() => handleClick(item)}
+                        >
+                            {item}
+                        </TabButton>
+                    );
+                })}
+            </LeftContent>
+        </Tab>
     );
 };
--- src/pages/media.js
+++ src/pages/media.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/dev-hub/layout';
 import Tab from '../components/dev-hub/tab';
+import useAllVideos from '../hooks/use-all-videos';
+import usePodcasts from '../hooks/use-podcasts';
+import { P } from '../components/dev-hub/text';
+import CardList from '../components/dev-hub//card-list';
 
 export default () => {
+    const { videos, error, isLoading } = useAllVideos();
+    const {
+        podcasts,
+        error: errorPodcasts,
+        isLoading: isLoadingPodcasts,
+    } = usePodcasts();
+
+    const [showContent, setShowContent] = useState('All');
+    const [activeItem, setActiveItem] = useState('All');
+
+    const display = content => {
+        setShowContent(content);
+        setActiveItem(content);
+    };
+
+    const tabTexts = ['All', 'Articles', 'Videos', 'Podcasts'];
+
     return (
         <Layout>
-            <Tab />
+            <Tab
+                handleClick={display}
+                tabTexts={tabTexts}
+                activeItem={activeItem}
+            />
+
+            {(isLoading || isLoadingPodcasts) && <P>Loading...</P>}
+
+            {/*TODO: display articles once we have access to them*/}
+
+            {showContent === 'All' && (
+                <CardList videos={videos} podcasts={podcasts} />
+            )}
+            {showContent === 'Videos' && <CardList videos={videos} />}
+            {showContent === 'Podcasts' && <CardList podcasts={podcasts} />}
+
+            {(error || errorPodcasts) && (
+                <P>Check back later for upcoming media contents</P>
+            )}
         </Layout>
     );
 };
