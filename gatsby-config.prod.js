88d764462fef5f6479bd2fdb6ddee7694837a267
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-05-06 15:42:03 -0400

    Update prod config

--- gatsby-config.prod.js
+++ gatsby-config.prod.js
@@ -1,19 +1,41 @@
 const { siteUrl } = require('./src/queries/site-url');
 const { getMetadata } = require('./src/utils/get-metadata');
 const { articleRssFeed } = require('./src/utils/setup/article-rss-feed');
+const {
+    DOCUMENTS_COLLECTION,
+    SNOOTY_STITCH_ID,
+} = require('./src/build-constants');
+const { constructDbFilter } = require('./src/utils/setup/construct-db-filter');
 
 require('dotenv').config({
     path: '.env.production',
 });
 
 const metadata = getMetadata();
+const PAGE_ID_PREFIX = `${metadata.project}/${metadata.user}/${metadata.parserBranch}`;
 
 module.exports = {
     pathPrefix: '',
     plugins: [
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-emotion',
-        'gatsby-source-mongodb-stitch',
+        {
+            resolve: 'gatsby-source-mongodb-stitch',
+            options: {
+                stitchId: SNOOTY_STITCH_ID,
+                functions: [
+                    {
+                        name: 'fetchDocuments',
+                        args: [
+                            metadata.database,
+                            DOCUMENTS_COLLECTION,
+                            constructDbFilter(PAGE_ID_PREFIX),
+                        ],
+                        resultType: 'StitchArticle',
+                    },
+                ],
+            },
+        },
         {
             resolve: 'gatsby-plugin-sitemap',
             options: {
