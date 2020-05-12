09922731b5bff628b705f93ba601e70193bacd02
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-05-12 11:22:00 -0400

    Fix related pages using refuri and fileid

--- src/utils/setup/get-related-pages-with-images.js
+++ src/utils/setup/get-related-pages-with-images.js
@@ -2,14 +2,20 @@ const dlv = require('dlv');
 
 const getRelatedPagesWithImages = (pageNodes, RESOLVED_REF_DOC_MAPPING) => {
     const related = dlv(pageNodes, 'query_fields.related', []);
-    const relatedPageInfo = related.map(r => ({
-        image: dlv(
-            RESOLVED_REF_DOC_MAPPING,
-            [r.target, 'query_fields', 'atf-image'],
-            null
-        ),
-        ...r,
-    }));
+    const relatedPageInfo = related.map(r => {
+        // Handle `reference` and `ref_role` types
+        const target = r.target || r.refuri || r.fileid;
+        const slug = target && target.slice(1, target.length);
+        return {
+            ...r,
+            target,
+            image: dlv(
+                RESOLVED_REF_DOC_MAPPING,
+                [slug, 'query_fields', 'atf-image'],
+                null
+            ),
+        };
+    });
     return relatedPageInfo;
 };
 
