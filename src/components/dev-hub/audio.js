8309586b8fa8a2ade380f42ae1f4110e294d9d2f
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-05-15 16:41:49 -0400

    Working player

--- src/components/dev-hub/audio.js
+++ src/components/dev-hub/audio.js
@@ -3,19 +3,21 @@ import ReactPlayer from 'react-player';
 import styled from '@emotion/styled';
 import Button from './button';
 import CardBadge from './card-badge';
-import { colorMap, layer } from './theme';
+import Slider from './slider';
+import { H2, P } from './text';
+import { colorMap, layer, size } from './theme';
 
 const ReactPlayerContainer = styled('div')`
-    background-color: ${colorMap.greyDarkTwo};
+    background-color: ${colorMap.greyDarkThree};
     position: fixed;
     bottom: 0;
     left: 0;
     width: 100%;
-    height: 80px;
+    ${({ isExpanded }) => !isExpanded && `height: 80px`};
     z-index: ${layer.superFront};
     display: flex;
     justify-content: space-around;
-    /* flex-direction: column; */
+    flex-direction: column;
     padding: 8px 100px;
 `;
 
@@ -30,7 +32,7 @@ const StyledPlayButton = styled(Button)`
 `;
 
 const StyledReactPlayer = styled(ReactPlayer)`
-    background-color: ${colorMap.greyDarkTwo};
+    background-color: ${colorMap.greyDarkThree};
 `;
 
 const StyledImage = styled('img')`
@@ -39,47 +41,58 @@ const StyledImage = styled('img')`
 
 const ContentContainer = styled('div')`
     display: flex;
+    align-self: center;
     align-items: center;
+    ${({ isExpanded }) => isExpanded && `align-items: flex-start`};
+
     width: 100%;
     max-width: 1400px;
     justify-content: space-between;
 `;
 
-const TEST_PODCAST = {
-    description: undefined,
-    mediaType: 'podcast',
-    publishDate: 'Wed, 06 May 2020 16:00:00 +0000',
-    thumbnailUrl:
-        'https://ssl-static.libsyn.com/p/assets/3/2/d/b/32dbb642e0f5c977/MDB_Podcast_Cover_Art_V2.png',
-    title: 'Ep. 6 Five Ways to Reduce Costs with MongoDB Atlas',
-    url:
-        'https://traffic.libsyn.com/secure/mongodb/Updated-5-Ways-Mike_-_4_13_20-b.mp3?dest-id=18542',
-};
-
-const ProgressBar = styled('input')`
+const StyledExpandedContainer = styled('div')`
+    background-color: ${colorMap.greyDarkThree};
+    display: flex;
+    padding-top: ${size.large};
+    align-self: center;
+    max-width: 1400px;
     width: 100%;
 `;
 
-const ProgressSlider = ({ onChange, percent, seconds, total }) => {
-    return (
-        <div style={{ display: 'flex' }}>
-            {`${Math.floor(seconds / 60)}:${Math.floor(seconds % 60)}`}
-            <ProgressBar
-                min="0"
-                max={total}
-                value={seconds}
-                type="range"
-                percent={percent}
-                onInput={() => onChange(seconds)}
-            />
-            {`${Math.floor(total / 60)}:${Math.floor(total % 60)}`}
-        </div>
-    );
+const ExpandedContainer = ({ podcast }) => (
+    <StyledExpandedContainer>
+        <H2>{podcast.title}</H2>
+        <P>{podcast.description}</P>
+    </StyledExpandedContainer>
+);
+
+const PlayOptionsContainer = styled('div')`
+    display: flex;
+    align-items: center;
+    width: 100%;
+`;
+const DescriptionContainer = styled('div')`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+const getTimeLabel = secondsElapsed => {
+    const minutes = Math.floor(secondsElapsed / 60);
+    const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const seconds = Math.floor(secondsElapsed % 60);
+    const displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+    return `${displayMinutes}:${displaySeconds}`;
 };
 
 // Pass in entire podcast
 // Trigger?
-const Audio = ({ onClose, isActive, podcast = TEST_PODCAST, ...props }) => {
+const Audio = ({ onClose, isActive, podcast, ...props }) => {
+    const [playerRef, setPlayerRef] = useState(null);
+    const [isExpanded, setIsExpanded] = useState(false);
+    const toggleIsExpanded = useCallback(() => {
+        setIsExpanded(!isExpanded);
+    }, [isExpanded]);
     const [isPlaying, setIsPlaying] = useState(true);
     const toggleIsPlaying = useCallback(
         e => {
@@ -88,39 +101,52 @@ const Audio = ({ onClose, isActive, podcast = TEST_PODCAST, ...props }) => {
         },
         [isPlaying]
     );
-    const [progress, setProgress] = useState({});
+    const [progress, setProgress] = useState({ playedSeconds: 0 });
     const [duration, setDuration] = useState(null);
-    // Progress gives playedSeconds, and played (fraction of total)
-    useEffect(() => {}, [progress.played, progress.playedSeconds]);
+    const onChange = useCallback(
+        seconds => {
+            playerRef.seekTo(seconds);
+        },
+        [playerRef]
+    );
     useEffect(() => {
         setIsPlaying(isActive);
     }, [isActive]);
     return isActive ? (
-        <ReactPlayerContainer {...props}>
-            <ContentContainer>
+        <ReactPlayerContainer isExpanded={isExpanded} {...props}>
+            <ContentContainer isExpanded={isExpanded}>
                 <StyledImage
                     height="64"
                     width="64"
                     src={podcast.thumbnailUrl}
                     alt={podcast.title}
                 />
-                {podcast.title}
-                <StyledPlayButton play onClick={toggleIsPlaying} />
-                <StyledReactPlayer
-                    width="0"
-                    height="0"
-                    url={podcast.url}
-                    onDuration={setDuration}
-                    onProgress={setProgress}
-                    playing={isPlaying}
-                />
-                <ProgressSlider
-                    onChange={console.log}
-                    percent={progress.played}
-                    seconds={progress.playedSeconds}
-                    total={duration}
-                />
-                <StyledCardBadge contentType="podcast" />
+                <DescriptionContainer>
+                    <PlayOptionsContainer>
+                        {podcast.title}
+                        <StyledPlayButton play onClick={toggleIsPlaying} />
+                        <StyledReactPlayer
+                            ref={setPlayerRef}
+                            width="0"
+                            height="0"
+                            url={podcast.url}
+                            onDuration={setDuration}
+                            onProgress={setProgress}
+                            playing={isPlaying}
+                        />
+                        <Slider
+                            current={progress.playedSeconds}
+                            currentLabel={getTimeLabel(progress.playedSeconds)}
+                            onChange={onChange}
+                            seconds={progress.playedSeconds}
+                            total={duration}
+                            totalLabel={getTimeLabel(duration)}
+                        />
+                        <StyledCardBadge contentType="podcast" />
+                    </PlayOptionsContainer>
+                    {isExpanded && <ExpandedContainer podcast={podcast} />}
+                </DescriptionContainer>
+                <Button onClick={toggleIsExpanded}>expand</Button>
                 <Button aria-label="close" onClick={onClose}>
                     &times;
                 </Button>
