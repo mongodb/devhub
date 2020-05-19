ee32fad0667aebe4ded8825f813be65b249f5d28
tahiya-mongo <tahiya.chowdhury@10gen.com>
2020-05-19 12:16:27 -0400

    Change font-size and line-height

--- src/components/dev-hub/section-header.js
+++ src/components/dev-hub/section-header.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { colorMap, lineHeight } from './theme';
-import { P } from './text';
+import { P4 } from './text';
 
 const SectionHeader = styled('div')`
     border-bottom: 1px solid ${colorMap.greyDarkOne};
     letter-spacing: 3px;
     text-transform: uppercase;
 `;
-const StyledP = styled(P)`
+const StyledP4 = styled(P4)`
     line-height: ${lineHeight.tiny};
 `;
 
 export default ({ children }) => (
     <SectionHeader>
-        <StyledP>{children}</StyledP>
+        <StyledP4>{children}</StyledP4>
     </SectionHeader>
 );
