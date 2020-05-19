af95d37a01c073f157a2ffcf9788cb9f6e9d7817
tahiya-mongo <tahiya.chowdhury@10gen.com>
2020-05-19 18:12:49 -0400

    Change text falvor based on design updates

--- src/components/dev-hub/section-header.js
+++ src/components/dev-hub/section-header.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { colorMap, lineHeight } from './theme';
-import { P4 } from './text';
+import { colorMap } from './theme';
+import { ArticleH2 } from './text';
 
 const SectionHeader = styled('div')`
     border-bottom: 1px solid ${colorMap.greyDarkOne};
-    letter-spacing: 3px;
-    text-transform: uppercase;
-`;
-const StyledP4 = styled(P4)`
-    line-height: ${lineHeight.tiny};
 `;
 
 export default ({ children }) => (
     <SectionHeader>
-        <StyledP4>{children}</StyledP4>
+        <ArticleH2>{children}</ArticleH2>
     </SectionHeader>
 );
