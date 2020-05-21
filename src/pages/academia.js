8107f1f95edf0ecc269dfc7a7eeffa7343f0826e
tahiya-mongo <tahiya.chowdhury@10gen.com>
2020-05-21 14:48:51 -0400

    Offerings section

--- src/pages/academia.js
+++ src/pages/academia.js
@@ -7,7 +7,6 @@ import {
     fontSize,
     screenSize,
     size,
-    lineHeight,
 } from '../components/dev-hub/theme';
 import { H3, ArticleH2, ArticleH3 } from '../components/dev-hub/text';
 import styled from '@emotion/styled';
@@ -28,18 +27,33 @@ const HeaderContent = styled('div')`
     max-width: ${HEADER_CONTENT_MAX_WIDTH};
 `;
 
-const Heading = styled(ArticleH2)`
+const Title = styled(ArticleH2)`
     font-size: ${fontSize.small};
     font-family: 'Fira Mono';
 `;
 
-const Description = styled(ArticleH3)`
+const StyledArticleH3 = styled(ArticleH3)`
     font-family: akzidenz;
     font-weight: normal;
 `;
 
 const StyledButton = styled(Button)`
     margin-top: ${size.large};
+`;
+
+const OfferingsSection = styled('div')`
+    // padding-left: 360px;
+    // display: flex;
+    // justify-content: center;
+    // flex-direction: column;
+    // margin: 0 auto;
+`;
+
+const OfferingsContent = styled('div')`
+    margin-bottom: ${size.xlarge};
+    max-width: 324px;
+    width: 100%;
+    margin: 0 auto;
 `;
 
 export default () => {
@@ -57,15 +71,15 @@ export default () => {
             <HeroBanner breadcrumb={academiaBreadcrumbs}>
                 <Header>
                     <HeaderContent>
-                        <Heading>MongoDB for Academia</Heading>
+                        <Title>MongoDB for Academia</Title>
 
-                        <H3>Educators are more important than ever</H3>
+                        <H3>Teach modern databases with MongoDB</H3>
 
-                        <Description>
-                            Gain access to teaching resources and materials
-                            created by MongoDB, and inspire the developer of
-                            tomorrow with the database for modern applications.
-                        </Description>
+                        <StyledArticleH3>
+                            Enrich your curriculum with MongoDB content and
+                            prepare the developers of tomorrow to build modern
+                            applications.
+                        </StyledArticleH3>
 
                         <StyledButton to="" primary hasArrow={false}>
                             Join MongoDB for Academia
@@ -73,6 +87,38 @@ export default () => {
                     </HeaderContent>
                 </Header>
             </HeroBanner>
+
+            <OfferingsSection>
+                <OfferingsContent>
+                    <ArticleH2>Teach MongoDB with Confidence</ArticleH2>
+                    <StyledArticleH3>
+                        Gain access to MongoDB software and curriculum content
+                        sourced from MongoDB education experts that easily
+                        integrates in your classroom teaching. We can also
+                        consult you with planning a curriculum.
+                    </StyledArticleH3>
+                </OfferingsContent>
+
+                <OfferingsContent>
+                    <ArticleH2>MongoDB University On-Demand</ArticleH2>
+                    <StyledArticleH3>
+                        You and your students will receive special on-demand
+                        access to MongoDB University, with cohort tracking,
+                        usage analytics in addition to great content that you
+                        can incorporate into your curriculum.
+                    </StyledArticleH3>
+                </OfferingsContent>
+
+                <OfferingsContent>
+                    <ArticleH2>
+                        Connect with Educators Around the World{' '}
+                    </ArticleH2>
+                    <StyledArticleH3>
+                        Access our MongoDB for Academia community to
+                        collaborate, share tips and get inspired.
+                    </StyledArticleH3>
+                </OfferingsContent>
+            </OfferingsSection>
         </Layout>
     );
 };
