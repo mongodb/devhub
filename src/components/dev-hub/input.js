afeb57328a2123a320708372ce4d0307c9182405
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-05-26 13:42:44 -0400

    Remove props from being passed twice on input

--- src/components/dev-hub/input.js
+++ src/components/dev-hub/input.js
@@ -89,7 +89,7 @@ const InputContainer = styled('div')`
 const FormInput = ({ narrow, value, ...props }) => {
     const isEmpty = !value;
     return (
-        <InputContainer isEmpty={isEmpty} narrow={narrow} {...props}>
+        <InputContainer isEmpty={isEmpty} narrow={narrow}>
             <Label
                 labelAbsoluteLeft={LABEL_ABSOLUTE_LEFT}
                 labelStartTop={LABEL_START_TOP}
