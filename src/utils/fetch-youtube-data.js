961c08ff9ad163562319e9d35bdc690db37deda6
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-06-01 13:58:03 -0400

    Use mongodb-stitch-server-sdk for build time calls

--- src/utils/fetch-youtube-data.js
+++ src/utils/fetch-youtube-data.js
@@ -1,27 +1,12 @@
-import dlv from 'dlv';
 import { requestYoutubePlaylist } from './devhub-api-stitch';
+import { simplifyYoutubeResponse } from './simplify-youtube-response';
 // Fetches data from youtube api
-
-const simplifyResponse = responseData => {
-    const video = responseData.snippet;
-    const youtubeJSON = {
-        mediaType: 'youtube',
-        title: video['title'],
-        publishDate: video['publishedAt'],
-        description: video['description'],
-        videoId: dlv(video, 'resourceId.videoId', []),
-        playlistId: video['playlistId'],
-        thumbnailUrl: dlv(video, 'thumbnails.standard.url', []),
-    };
-
-    return youtubeJSON;
-};
 
 const fetchYoutubeData = async (maxResults = 5) => {
     try {
         const response = await requestYoutubePlaylist(maxResults);
         if (response) {
-            const videoList = response.items.map(simplifyResponse);
+            const videoList = response.items.map(simplifyYoutubeResponse);
             return videoList;
         }
     } catch (e) {
--- src/utils/setup/init-stich.js
+++ src/utils/setup/init-stich.js
@@ -1,10 +1,10 @@
 import { Stitch, AnonymousCredential } from 'mongodb-stitch-server-sdk';
 import { SNOOTY_STITCH_ID } from '../../build-constants';
 
-export const initStitch = async () => {
-    const stitchClient = Stitch.hasAppClient(SNOOTY_STITCH_ID)
-        ? Stitch.getAppClient(SNOOTY_STITCH_ID)
-        : Stitch.initializeAppClient(SNOOTY_STITCH_ID);
+export const initStitch = async (id = SNOOTY_STITCH_ID) => {
+    const stitchClient = Stitch.hasAppClient(id)
+        ? Stitch.getAppClient(id)
+        : Stitch.initializeAppClient(id);
     await stitchClient.auth
         .loginWithCredential(new AnonymousCredential())
         .catch(console.error);
