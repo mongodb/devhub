28042462a97e34630e5742157f60b638c96cded8
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-06-02 16:12:50 -0400

    Add back in use of youtube publish date

--- src/utils/simplify-youtube-response.js
+++ src/utils/simplify-youtube-response.js
@@ -2,10 +2,17 @@ import dlv from 'dlv';
 
 export const simplifyYoutubeResponse = responseData => {
     const video = responseData.snippet;
+    // Video publish date is stored in "contentDetails", publish date in "snippet"
+    // is for when this video was added to the playlist
+    const publishedDate = dlv(
+        responseData,
+        'contentDetails.videoPublishedAt',
+        null
+    );
     const youtubeJSON = {
         mediaType: 'youtube',
         title: video['title'],
-        publishDate: video['publishedAt'],
+        publishDate: publishedDate,
         description: video['description'],
         videoId: dlv(video, 'resourceId.videoId', []),
         playlistId: video['playlistId'],
