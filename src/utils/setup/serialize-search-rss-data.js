0c27d09fd96ba0dd012ffa960ac17f64382ee34c
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-09-01 15:13:48 -0400

    Refactor logic for handling nested XML

--- src/utils/setup/serialize-search-rss-data.js
+++ src/utils/setup/serialize-search-rss-data.js
@@ -1,29 +1,43 @@
+// We want to omit empty fields from the XML entirely
+const handlePossiblyEmptyField = (
+    article,
+    fieldName,
+    tagName,
+    getField = x => x
+) =>
+    article[fieldName]
+        ? [...article[fieldName].map(a => ({ [tagName]: getField(a) }))]
+        : null;
+
 const getCustomRSSElements = article => {
-    const authorNames = article.authors
-        ? [...article.authors.map(a => ({ author_name: a.name }))]
-        : [];
-    const languages = article.languages
-        ? [...article.languages.map(l => ({ language: l }))]
-        : [];
-    const products = article.products
-        ? [...article.products.map(l => ({ product: l }))]
-        : [];
-    const tags = article.tags ? [...article.tags.map(t => ({ tag: t }))] : [];
+    const authorNames = handlePossiblyEmptyField(
+        article,
+        'authors',
+        'author_name',
+        a => a.name
+    );
+    const languages = handlePossiblyEmptyField(
+        article,
+        'languages',
+        'language'
+    );
+    const products = handlePossiblyEmptyField(article, 'products', 'product');
+    const tags = handlePossiblyEmptyField(article, 'tags', 'tag');
     const customElements = [
         { atf_image: article.atfimage },
         { slug: article.slug },
         { type: article.type },
     ];
-    if (authorNames.length) {
+    if (authorNames) {
         customElements.push({ author_names: authorNames });
     }
-    if (languages.length) {
+    if (languages) {
         customElements.push({ languages: languages });
     }
-    if (products.length) {
+    if (products) {
         customElements.push({ products: products });
     }
-    if (tags.length) {
+    if (tags) {
         customElements.push({ tags: tags });
     }
     return customElements;
