ac95a4d9a94830320c3057705ceb5d8cf2224e0f
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-10-19 10:05:56 -0400

    Add new image assets and ensure responsive behavior

--- src/components/dev-hub/mongodb-live-banner.js
+++ src/components/dev-hub/mongodb-live-banner.js
@@ -3,24 +3,29 @@ import styled from '@emotion/styled';
 import useMedia from '../../hooks/use-media';
 import Image from '../Image';
 import Link from './link';
-import { screenSize } from './theme';
+import { screenSize, size } from './theme';
 
 const LiveImage = styled(Image)`
     border-radius: 0;
     margin-bottom: 0;
     width: 100%;
+    @media ${screenSize.mediumAndUp} {
+        min-height: ${size.large};
+        object-fit: cover;
+    }
 `;
 
 const MongodbLiveBanner = () => {
     const isMobile = useMedia(screenSize.upToMedium);
+    // TODO: Update link below for new .live series
     return (
         <Link href="https://www.mongodb.com/resources/channel/mongodblive?tck=devhubbannerdotlive">
             <LiveImage
                 alt="Click to watch keynotes and sessions from MongoDB.live, our virtual developer conference."
                 src={
                     isMobile
-                        ? '/public/images/post-live-sm.png'
-                        : '/public/images/post-live-lg.png'
+                        ? '/public/images/dot-live-banner-mobile.png'
+                        : '/public/images/dot-live-banner-desktop.png'
                 }
             />
         </Link>
