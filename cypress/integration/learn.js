008135f79c6ef1974e63991d44b646b229b4f2d7
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-11-06 16:17:10 -0500

    DEVHUB-251: Sort CardList by created-date

--- cypress/integration/learn.js
+++ cypress/integration/learn.js
@@ -1,3 +1,10 @@
+const SECOND_ARTICLE_IN_ORDERING =
+    '/article/build-newsletter-website-mongodb-data-platform';
+const FIRST_ARTICLE_IN_ORDERING = '/how-to/transactions-c-dotnet';
+const FIRST_ARTICLE_UPDATED_DATE = 'Oct 03, 2020';
+const FIRST_ARTICLE_PUBLISHED_DATE = 'Oct 17, 2018';
+const SECOND_ARTICLE_PUBLISHED_DATE = 'Apr 21, 2020';
+
 describe('Learn Page', () => {
     it('should properly render the learn page', () => {
         cy.visitWithoutFetch('/learn');
@@ -18,7 +25,31 @@ describe('Learn Page', () => {
                 cy.checkSecondaryFeaturedArticleCard
             );
     });
+    it('should sort using the updated date where appropriate', () => {
+        cy.toggleLearnPageTab('Articles');
+        cy.get('[data-test="card-list"]').within(() => {
+            cy.get('[data-test="card"]').first().click();
+        });
+        cy.url().should('include', FIRST_ARTICLE_IN_ORDERING);
+        // By targeting the hero banner we can be sure the navigation is done
+        cy.get('[data-test="hero-banner"]').within(() => {
+            cy.contains(`Updated: ${FIRST_ARTICLE_UPDATED_DATE}`);
+            cy.contains(`Published: ${FIRST_ARTICLE_PUBLISHED_DATE}`);
+        });
+        cy.visitWithoutFetch('/learn');
+        cy.toggleLearnPageTab('Articles');
+        cy.get('[data-test="card-list"]').within(() => {
+            cy.get('[data-test="card"]').eq(1).click();
+        });
+        cy.url().should('include', SECOND_ARTICLE_IN_ORDERING);
+        cy.get('[data-test="hero-banner"]').within(() => {
+            // This article has no updated date, so it should fallback to the publish date for sorting, which should be before the above article
+            cy.contains('Updated: ').should('not.exist');
+            cy.contains(`Published: ${SECOND_ARTICLE_PUBLISHED_DATE}`);
+        });
+    });
     it('should filter content based on the selected tab', () => {
+        cy.visitWithoutFetch('/learn');
         // TODO: Check content in "All" (Stub videos and podcasts)
         // Check content in "Articles"
         cy.toggleLearnPageTab('Articles');
@@ -27,7 +58,7 @@ describe('Learn Page', () => {
                 .first()
                 .within(card => {
                     cy.checkArticleCard(card);
-                    cy.contains('Build a Newsletter Website');
+                    cy.contains('Working with MongoDB Transactions');
                 });
         });
         // TODO Check content in "Videos" (Stub videos and podcasts)
@@ -35,6 +66,7 @@ describe('Learn Page', () => {
         // TODO Check content in "Podcasts" (Stub videos and podcasts)
         cy.toggleLearnPageTab('Podcasts');
     });
+
     it('should only show the filter bar for "All" or "Articles"', () => {
         cy.toggleLearnPageTab('All');
         cy.get('[data-test="filter-bar"]').should('exist');
@@ -47,7 +79,7 @@ describe('Learn Page', () => {
     });
     it('should filter content using the filter dropdowns', () => {
         cy.toggleLearnPageTab('Articles');
-        cy.checkFirstCardInCardList('Build a Newsletter Website');
+        cy.checkFirstCardInCardList('Working with MongoDB Transactions');
         cy.get('[data-test="filter-bar"]').within(() => {
             cy.get('[role="listbox"]').first().click();
         });
@@ -58,7 +90,7 @@ describe('Learn Page', () => {
         // The url should contain the filter value as a param
         cy.url().should('include', '?products=Atlas');
         // Check content
-        cy.checkFirstCardInCardList('How to work with Johns Hopkins');
+        cy.checkFirstCardInCardList('Coronavirus Map');
     });
     it('should have a list of item cards', () => {
         cy.get('[data-test="card-list"]').within(() => {
