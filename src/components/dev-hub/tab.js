7a3bf45dc6e01950b9d90653211a124c774dbece
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-11-30 11:30:26 -0500

    Make it simple

--- src/components/dev-hub/tab.js
+++ src/components/dev-hub/tab.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
-import ClientOnly from '../ClientOnly';
 import { size, fontSize, screenSize } from './theme';
 import { P } from './text';
 
@@ -43,28 +42,35 @@ const TabButton = styled('button')`
     }
 `;
 
-const mapTabTextToButton = (textList, activeItem, handleClick) =>
-    textList.map(item => {
-        const isActive = item === activeItem;
-        return (
-            <TabButton
-                data-test={`tab-${item}`}
-                key={item}
-                isActive={isActive}
-                onClick={() => handleClick(item)}
-            >
-                <P collapse>{item}</P>
-            </TabButton>
-        );
-    });
-
-const Tabs = ({ activeItem, className, handleClick, leftTabs, rightTabs }) => (
-    <ClientOnly>
-        <Tab data-test="tabs" className={className}>
-            <div>{mapTabTextToButton(leftTabs, activeItem, handleClick)}</div>
-            <div>{mapTabTextToButton(rightTabs, activeItem, handleClick)}</div>
-        </Tab>
-    </ClientOnly>
+const mapTabTextToButton = (textList, activeItem, handleClick) => (
+    <div>
+        {textList.map(item => {
+            const isActive = item === activeItem;
+            return (
+                <TabButton
+                    data-test={`tab-${item}`}
+                    key={item}
+                    isActive={isActive}
+                    onClick={() => handleClick(item)}
+                >
+                    <P collapse>{item}</P>
+                </TabButton>
+            );
+        })}
+    </div>
 );
+
+const Tabs = ({ activeItem, className, handleClick, leftTabs, rightTabs }) => {
+    const LeftTabs = () =>
+        mapTabTextToButton(leftTabs, activeItem, handleClick);
+    const RightTabs = () =>
+        mapTabTextToButton(rightTabs, activeItem, handleClick);
+    return (
+        <Tab data-test="tabs" className={className}>
+            <LeftTabs />
+            <RightTabs />
+        </Tab>
+    );
+};
 
 export default Tabs;
