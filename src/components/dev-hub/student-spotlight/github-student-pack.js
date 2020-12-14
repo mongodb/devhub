de41ed4a1709578f3aa8a6920e260d4d6bd68f61
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-12-14 14:57:34 -0500

    Begin adding backpack component

---
+++ src/components/dev-hub/student-spotlight/github-student-pack.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MediaBlock from '../media-block';
+import { P } from '../text';
+import GithubBackpackMongo from '../../../images/github-backpack-mongo.png';
+
+const GithubStudentPack = () => (
+    <MediaBlock mediaComponent={GithubBackpackMongo}>
+        <P>Foo bar</P>
+    </MediaBlock>
+);
+
+export default GithubStudentPack;
