186a886e3c5ddc851d807ac067a160bbe4dfec7b
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-12-15 11:00:12 -0500

    Let captions float with images in figures

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -2,20 +2,31 @@ import React from 'react';
 import styled from '@emotion/styled';
 import CaptionLegend from './CaptionLegend';
 import Image from './Image';
+import { getImageAlignmentStyle } from '../utils/get-image-alignment-style';
 import { getNestedValue } from '../utils/get-nested-value';
 import { size } from './dev-hub/theme';
 
 const Figure = styled('figure')`
     margin: 0 0 ${size.articleContent};
+    width: ${({ figWidth }) => figWidth};
+    ${({ alignStyle }) => alignStyle};
 `;
-export default ({ nodeData, ...rest }) => (
-    <Figure
-        className="figure"
-        style={{
-            width: getNestedValue(['options', 'figwidth'], nodeData) || 'auto',
-        }}
-    >
-        <Image nodeData={nodeData} captioned={!!nodeData.children.length} />
-        <CaptionLegend {...rest} nodeData={nodeData} />
-    </Figure>
-);
+
+const removeAlignOption = nodeData => delete nodeData['options']['align'];
+
+export default ({ nodeData, ...rest }) => {
+    const customAlign = getNestedValue(['options', 'align'], nodeData);
+    if (customAlign) {
+        // Apply the alignment to the figure, no need to also apply to image
+        removeAlignOption(nodeData);
+    }
+    const alignStyle = getImageAlignmentStyle(customAlign);
+    const figWidth =
+        getNestedValue(['options', 'figwidth'], nodeData) || 'auto';
+    return (
+        <Figure alignStyle={alignStyle} figWidth={figWidth}>
+            <Image nodeData={nodeData} captioned={!!nodeData.children.length} />
+            <CaptionLegend {...rest} nodeData={nodeData} />
+        </Figure>
+    );
+};
