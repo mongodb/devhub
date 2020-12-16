4082e42310cd542b6d4898f518817b93149fcfe6
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-12-16 11:33:06 -0500

    Add scalable grid MVP with new story

--- src/components/dev-hub/grid.js
+++ src/components/dev-hub/grid.js
@@ -1,36 +1,61 @@
 import React, { useCallback, useMemo } from 'react';
 import { css } from '@emotion/core';
 import styled from '@emotion/styled';
+import { size } from './theme';
 
 const GridContainer = styled('div')`
     display: grid;
-    grid-template-columns: repeat(${({ layoutCols }) => layoutCols}, 1fr);
-    grid-template-rows: repeat(${({ layoutRows }) => layoutRows}, 1fr);
+    grid-template-columns: repeat(
+        ${({ layoutCols }) => layoutCols},
+        ${({ colWidth }) => colWidth}
+    );
+    grid-template-rows: repeat(
+        ${({ layoutRows }) => layoutRows},
+        ${({ rowHeight }) => rowHeight}
+    );
+    gap: ${size.default};
 `;
 
 const gridSpan = colSpan => css`
     grid-column-end: span ${colSpan};
 `;
 
-// Wrapper component which takes a layout and constructs a grid with it
-const Grid = ({ children, layout, ...props }) => {
+const getPosition = (i, layout) => {
+    let count = 0;
+    let index = -1;
+    while (i >= count) {
+        index++;
+        if (i < count + layout[index].length) {
+            return layout[index][i - count];
+        }
+        count += layout[index].length;
+    }
+};
+
+// Wrapper component which takes a [[layout]] and constructs a grid with it
+const Grid = ({
+    children,
+    layout,
+    colWidth = '1fr',
+    rowHeight = '1fr',
+    ...props
+}) => {
     const layoutRows = layout.length;
     const layoutCols = layout[0].reduce((a, b) => a + b, 0);
-    const layoutPosition = useCallback(i => layout[0][i % layout[0].length], [
-        layout,
-    ]);
     const gridElements = useMemo(
         () =>
             children.map((child, i) =>
                 React.cloneElement(child, {
-                    css: gridSpan(layoutPosition(i)),
+                    css: gridSpan(getPosition(i, layout)),
                     key: i,
                 })
             ),
-        [children, layoutPosition]
+        [children, layout]
     );
     return (
         <GridContainer
+            colWidth={colWidth}
+            rowHeight={rowHeight}
             layoutRows={layoutRows}
             layoutCols={layoutCols}
             {...props}
