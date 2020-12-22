5d16f934f8343677b0a69a4146fb036ae15827f6
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-12-22 11:10:36 -0500

    Split per item check

--- src/utils/setup/handle-create-home-page.js
+++ src/utils/setup/handle-create-home-page.js
@@ -3,27 +3,34 @@ import { getItemTypeFromUrl } from './get-item-type-from-url';
 
 const MAX_HOME_PAGE_FEATURED_ARTICLES = 4;
 
+const findFeaturedItemDetails = (item, allArticles) => {
+    // Currently, only articles are supported here
+    // TODO: Support items of all content types
+    const itemType = getItemTypeFromUrl(item);
+    switch (itemType) {
+        case 'article':
+            const article = findArticleWithSlug(allArticles, item);
+            if (article) {
+                return article;
+            } else {
+                throw new Error(`Featured article not found: ${item}`);
+            }
+        default:
+            throw new Error(`Featured article not found: ${item}`);
+    }
+};
+
 const getFeaturedItems = (homeFeaturedArticles, allArticles) => {
     const featuredItems = [];
     homeFeaturedArticles
         .slice(0, MAX_HOME_PAGE_FEATURED_ARTICLES)
         .forEach(item => {
-            // Currently, only articles are supported here
-            // TODO: Support items of all content types
-            const itemType = getItemTypeFromUrl(item);
-            switch (itemType) {
-                case 'article':
-                    const article = findArticleWithSlug(allArticles, item);
-                    if (article) {
-                        featuredItems.push(article);
-                    } else {
-                        throw new Error(`Featured article not found: ${item}`);
-                    }
-                    break;
-                default:
-                    throw new Error(`Featured article not found: ${item}`);
+            const featuredItem = findFeaturedItemDetails(item, allArticles);
+            if (featuredItem) {
+                featuredItems.push(featuredItem);
             }
         });
+    return featuredItems;
 };
 
 export const handleCreateHomePage = async (
