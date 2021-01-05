2b3ef0a6825d1f375bc7cd3864e2b3fe280aebd2
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-01-05 15:44:04 -0500

    Import CTA and student pack pieces

--- src/pages/academia/students/index.js
+++ src/pages/academia/students/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Layout from '~components/dev-hub/layout';
+import {
+    GithubStudentPack,
+    ShareProjectCTA,
+} from '~components/dev-hub/student-spotlight';
 import GalleryHeroBanner from '~components/pages/students/gallery-hero-banner';
 
 const Students = () => {
     return (
         <Layout>
             <GalleryHeroBanner />
+            <ShareProjectCTA />
+            <GithubStudentPack />
         </Layout>
     );
 };
