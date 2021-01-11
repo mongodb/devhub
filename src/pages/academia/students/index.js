f3f7fdd0cbba7a42f0a48216a588f85145927041
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-01-11 15:52:18 -0500

    Constrain content

--- src/pages/academia/students/index.js
+++ src/pages/academia/students/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import Layout from '~components/dev-hub/layout';
 import {
     GithubStudentPack,
@@ -7,14 +8,27 @@ import {
 import FeaturedProject from '~components/pages/students/featured-project';
 import AllProjects from '~components/pages/students/all-projects';
 import GalleryHeroBanner from '~components/pages/students/gallery-hero-banner';
+import { screenSize, size } from '~components/dev-hub/theme';
+
+const ContentContainer = styled('div')`
+    padding-left: ${size.xxlarge};
+    padding-right: ${size.xxlarge};
+    @media ${screenSize.upToLarge} {
+        /* Show image as child under breadcrumbs instead */
+        padding: ${size.medium};
+        width: 100%;
+    }
+`;
 
 const Students = () => (
     <Layout>
         <GalleryHeroBanner />
-        <FeaturedProject />
-        <AllProjects />
-        <ShareProjectCTA />
-        <GithubStudentPack />
+        <ContentContainer>
+            <FeaturedProject />
+            <AllProjects />
+            <ShareProjectCTA />
+            <GithubStudentPack />
+        </ContentContainer>
     </Layout>
 );
 
