f8a58bb82a24eba73304fe6f65d82caf88c43a73
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-01-15 15:24:27 -0500

    Begin adding copy and layout

---
+++ src/components/pages/academia/students-educators-details.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import GreenBulletedList from '../educators/green-bulleted-list';
+//TODO: Replace image with Visual Design asset once available
+import HeroBannerImage from '~images/1x/Academia.svg';
+import { H5 } from '~components/dev-hub/text';
+
+const BenefitCardLayout = styled('div')`
+    display: flex;
+    flex: 1;
+`;
+
+const BenefitCardsLayout = styled('div')`
+    display: flex;
+`;
+
+const BenefitTypeCard = ({ bullets, image }) => (
+    <BenefitCardLayout>
+        <img width="110px" height="92px" alt="" src={image} />
+        <div>
+            <H5>For Students</H5>
+            <GreenBulletedList children={bullets} />
+        </div>
+    </BenefitCardLayout>
+);
+
+const StudentsEducatorsDetails = () => (
+    <BenefitCardsLayout>
+        <BenefitTypeCard
+            image={HeroBannerImage}
+            bullets={[
+                'Join our GitHub Student Developer Pack offer',
+                'MongoDB Atlas: create a free Tier, or use $200 in Credits',
+                'MongoDB University On-Demand and Free Certification',
+            ]}
+        />
+        <BenefitTypeCard
+            image={HeroBannerImage}
+            bullets={[
+                'Access MongoDB course material & content support',
+                'On-demand access to MongoDB University, cohort tracking and usage analytics',
+                'Access our MongoDB for Academia community to collaborate, share tips and get inspired',
+            ]}
+        />
+    </BenefitCardsLayout>
+);
+
+export default StudentsEducatorsDetails;
