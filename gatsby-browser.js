5a098112d5c5033bdab1189e5495f3c1d15fb850
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-01-20 10:10:30 -0500

    Pull out new page logic for scroll update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,14 @@
+import dlv from 'dlv';
 import { removeTrailingSlash } from './src/utils/remove-trailing-slash';
 
+const isNewPage = (prevLocation, newLocation) => {
+    const prevPathname = removeTrailingSlash(prevLocation.pathname);
+    const newPathname = removeTrailingSlash(newLocation.pathname);
+    return prevPathname !== newPathname;
+};
+
 export const shouldUpdateScroll = ({ prevRouterProps, routerProps }) => {
-    const prevLocation =
-        prevRouterProps &&
-        removeTrailingSlash(prevRouterProps.location.pathname);
-    const newLocation =
-        routerProps && removeTrailingSlash(routerProps.location.pathname);
-    return prevLocation !== newLocation;
+    const prevLocation = dlv(prevRouterProps, ['location'], {});
+    const newLocation = dlv(routerProps, ['location'], {});
+    return isNewPage(prevLocation, newLocation);
 };
