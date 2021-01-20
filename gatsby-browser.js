081e5553a4a5ea59e520d63af0eb9b87224cf869
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-01-20 10:44:14 -0500

    DEVHUB-397 [Part 3]: Ensure Page Scroll if Anchor Link Exists (#605)
    * Pull out new page logic for scroll update
    
    * Move checks into separate modules
    
    * Add test for checking if a new page
    
    * Add test for checking anchor links

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,9 @@
-import { removeTrailingSlash } from './src/utils/remove-trailing-slash';
+import dlv from 'dlv';
+import { hasAnchorLink } from '~utils/has-anchor-link';
+import { isNewPage } from '~utils/is-new-page';
 
 export const shouldUpdateScroll = ({ prevRouterProps, routerProps }) => {
-    const prevLocation =
-        prevRouterProps &&
-        removeTrailingSlash(prevRouterProps.location.pathname);
-    const newLocation =
-        routerProps && removeTrailingSlash(routerProps.location.pathname);
-    return prevLocation !== newLocation;
+    const prevLocation = dlv(prevRouterProps, ['location'], {});
+    const newLocation = dlv(routerProps, ['location'], {});
+    return isNewPage(prevLocation, newLocation) || hasAnchorLink(newLocation);
 };
