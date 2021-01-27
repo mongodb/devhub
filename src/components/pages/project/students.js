e29274999750cf85ee1e0b784979f2a6ccb53319
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-01-27 15:32:47 -0500

    Clean up CSS

--- src/components/pages/project/students.js
+++ src/components/pages/project/students.js
@@ -8,29 +8,40 @@ import LinkedinIcon from '~components/dev-hub/icons/linkedin';
 import YoutubeIcon from '~components/dev-hub/icons/youtube';
 import AuthorImage from '~components/dev-hub/author-image';
 import { P3 } from '~components/dev-hub/text';
+import { size } from '~components/dev-hub/theme';
 import Link from '~components/Link';
 
+const AUTHOR_IMAGE_SIZE = 32;
+const AUTHOR_IMAGE_GRADIENT_WIDTH = 4;
+const CONDENSED_STUDENT_HEIGHT = '46px';
+const TOTAL_AUTHOR_IMAGE_SIZE = `${
+    AUTHOR_IMAGE_SIZE + AUTHOR_IMAGE_GRADIENT_WIDTH
+}px`;
+const ICON_SIZE = size.stripUnit(size.default);
+
 const StudentToggle = styled(Button)`
-    display: grid;
-    grid-template-columns: 32px 1fr 16px;
-    grid-template-rows: 56px;
-    column-gap: 16px;
     align-items: center;
-    width: 100%;
-    padding: 0;
-    text-decoration: none;
+    column-gap: ${size.default};
+    display: grid;
     font-family: inherit;
+    grid-template-columns: ${TOTAL_AUTHOR_IMAGE_SIZE} 1fr ${size.default};
+    grid-template-rows: ${CONDENSED_STUDENT_HEIGHT};
+    padding: 0;
     text-align: left;
+    text-decoration: none;
+    width: 100%;
 `;
 const InvisibleLink = styled(Link)`
-    text-decoration: none;
     margin-bottom: 10px;
+    text-decoration: none;
 `;
 const StudentLi = styled('li')`
     border-bottom: 1px solid ${({ theme }) => theme.colorMap.greyDarkThree};
     display: flex;
     justify-content: center;
     flex-direction: column;
+    padding-bottom: 10px;
+    margin-bottom: 10px;
 `;
 const StudentsList = styled('ul')`
     list-style: none;
@@ -43,10 +54,10 @@ const StudentBio = styled(P3)`
     color: ${({ theme }) => theme.colorMap.greyLightTwo};
 `;
 const LinkContainer = styled('div')`
-    display: grid;
-    grid-template-columns: 16px 1fr;
-    column-gap: 8px;
     align-items: center;
+    display: grid;
+    column-gap: ${size.xsmall};
+    grid-template-columns: ${size.default} 1fr;
     padding-bottom: 10px;
 `;
 const LinkText = styled(P3)`
@@ -55,69 +66,49 @@ const LinkText = styled(P3)`
     text-overflow: ellipsis;
     white-space: nowrap;
 `;
-const StudentInfo = styled('div')`
-    padding-bottom: 6px;
-`;
 
-const SocialMediaEntry = ({ Icon, url }) =>
-    url ? (
+const SocialMediaEntry = ({ Icon, url }) => {
+    const theme = useTheme();
+    return url ? (
         <InvisibleLink to={url}>
             <LinkContainer>
-                {Icon}
+                <Icon color={theme.colorMap.darkGreen} width={ICON_SIZE} />
                 <LinkText collapse>{url}</LinkText>
             </LinkContainer>
         </InvisibleLink>
     ) : null;
+};
+
+const SocialMediaEntries = ({ student }) => (
+    <>
+        <SocialMediaEntry Icon={GithubIcon} url={student.github_url} />
+        <SocialMediaEntry Icon={LinkedinIcon} url={student.linkedin_url} />
+        <SocialMediaEntry Icon={YoutubeIcon} url={student.youtube_url} />
+    </>
+);
 
 const Student = ({ student }) => {
-    const theme = useTheme();
     const [isOpen, setIsOpen] = useState(false);
     const toggleIsOpen = useCallback(() => setIsOpen(!isOpen), [isOpen]);
     return (
         <StudentLi>
             <StudentToggle onClick={toggleIsOpen}>
                 <AuthorImage
-                    gradientOffset={4}
+                    gradientOffset={AUTHOR_IMAGE_GRADIENT_WIDTH}
                     hideOnMobile={false}
-                    height={32}
-                    width={32}
+                    height={AUTHOR_IMAGE_SIZE}
+                    width={AUTHOR_IMAGE_SIZE}
                     image={student.image.url}
                     key={student.name}
                 />
                 <StudentName collapse>{student.name}</StudentName>
-                <ArrowheadIcon size="16" />
+                <ArrowheadIcon size={ICON_SIZE} />
             </StudentToggle>
             {isOpen && (
-                <StudentInfo>
+                <div>
                     <StudentBio>{student.bio}</StudentBio>
-                    <SocialMediaEntry
-                        Icon={
-                            <GithubIcon
-                                color={theme.colorMap.darkGreen}
-                                width="16"
-                            />
-                        }
-                        url={student.github_url}
-                    />
-                    <SocialMediaEntry
-                        Icon={
-                            <LinkedinIcon
-                                color={theme.colorMap.darkGreen}
-                                width="16"
-                            />
-                        }
-                        url={student.linkedin_url}
-                    />
-                    <SocialMediaEntry
-                        Icon={
-                            <YoutubeIcon
-                                color={theme.colorMap.darkGreen}
-                                width="16"
-                            />
-                        }
-                        url={student.youtube_url}
-                    />
-                </StudentInfo>
+                    <SocialMediaEntries student={student} />
+                </div>
             )}
         </StudentLi>
     );
