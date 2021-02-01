30a0dc2ce4b77e3018cff16ba84ddad47293cb1c
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-02-01 16:03:51 -0500

    Add trailing slash to Link uses

--- src/utils/add-trailing-slash-if-missing.js
+++ src/utils/add-trailing-slash-if-missing.js
@@ -4,3 +4,18 @@ export const addTrailingSlashIfMissing = url => {
     }
     return `${url}/`;
 };
+
+// More complicated function to add a trailing slash but before any query params
+// or anchor links
+export const addTrailingSlashBeforeParams = path => {
+    if (path.includes('?')) {
+        // Query params
+        const pieces = path.split('?');
+        return addTrailingSlashIfMissing(pieces[0]) + `?${pieces[1]}`;
+    } else if (path.includes('#')) {
+        // Anchor without query params
+        const pieces = path.split('#');
+        return addTrailingSlashIfMissing(pieces[0]) + `#${pieces[1]}`;
+    }
+    return addTrailingSlashIfMissing(path);
+};
--- src/utils/get-article-share-links.js
+++ src/utils/get-article-share-links.js
@@ -1,15 +1,17 @@
+import { addTrailingSlashIfMissing } from './add-trailing-slash-if-missing';
 import { removeTrailingSlash } from './remove-trailing-slash';
 
 export const getArticleShareLinks = (title, url) => {
+    const urlWithTrailingSlash = addTrailingSlashIfMissing(url);
     const urlWithoutTrailingSlash = removeTrailingSlash(url);
     const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${urlWithoutTrailingSlash}`;
     const twitterUrl = `https://twitter.com/intent/tweet?url=${urlWithoutTrailingSlash}&text=${encodeURIComponent(
         title
     )}`;
     // LinkedIn throws redirects to the same exact URL without a trailing slash
-    const linkedInUrl = `https://www.linkedin.com/shareArticle?url=${urlWithoutTrailingSlash}/`;
+    const linkedInUrl = `https://www.linkedin.com/shareArticle?url=${urlWithTrailingSlash}`;
     return {
-        articleUrl: urlWithoutTrailingSlash,
+        articleUrl: urlWithTrailingSlash,
         facebookUrl,
         linkedInUrl,
         twitterUrl,
