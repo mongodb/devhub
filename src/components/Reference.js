d72e9c782437adb317a38231c94d073fb8192a01
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-02-01 16:52:44 -0500

    Make Reference internal links actually internal

--- src/components/Reference.js
+++ src/components/Reference.js
@@ -3,11 +3,30 @@ import PropTypes from 'prop-types';
 import Link from './Link';
 import { getNestedValue } from '../utils/get-nested-value';
 
-const Reference = ({ nodeData }) => (
-    <Link className="reference" to={nodeData.refuri} target="_blank">
-        {getNestedValue(['children', 0, 'value'], nodeData)}
-    </Link>
-);
+const makeLinkInternalIfApplicable = link => {
+    if (!link) {
+        return link;
+    }
+    const linkIncludesDevHub = link.includes('https://developer.mongodb.com');
+    const linkGoesToForums = link.includes(
+        'https://developer.mongodb.com/community/forums'
+    );
+    if (linkIncludesDevHub && !linkGoesToForums) {
+        // Forums is technically "external" from an app standpoint, so we leave
+        // that one alone
+        return link.replace('https://developer.mongodb.com', '');
+    }
+    return link;
+};
+
+const Reference = ({ nodeData }) => {
+    const link = makeLinkInternalIfApplicable(nodeData.refuri);
+    return (
+        <Link className="reference" to={link} target="_blank">
+            {getNestedValue(['children', 0, 'value'], nodeData)}
+        </Link>
+    );
+};
 
 Reference.propTypes = {
     nodeData: PropTypes.shape({
