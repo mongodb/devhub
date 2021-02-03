12057c41a7281b7dc091943db5daaeafba438356
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-02-03 17:55:28 -0500

    Add some backgrounds, margins

--- src/components/pages/student-submit/form.js
+++ src/components/pages/student-submit/form.js
@@ -5,92 +5,102 @@ import Input from '~components/dev-hub/input';
 import TextArea from '~components/dev-hub/text-area';
 import { H5 } from '~components/dev-hub/text';
 
+const Centered = styled('div')`
+    display: flex;
+    justify-content: center;
+`;
+
 const H5ChangesOnClose = styled(H5)`
     ${({ isClosed, theme }) =>
         isClosed && `color: ${theme.colorMap.greyDarkOne}`};
+    float: left;
+    ${({ isClosed }) => isClosed && 'margin-bottom: 0'};
 `;
 const Legend = H5ChangesOnClose.withComponent('legend');
 
 const Fieldset = styled('fieldset')`
-    border: none;
-    > * {
+    background: #0c1c27;
+    border: 1px solid #21313c;
+    border-radius: 8px;
+    padding: 32px 48px;
+    max-width: 792px;
+    margin: 0 auto;
+    :not(:last-of-type) {
         margin-bottom: 24px;
     }
 `;
 
 const FieldsetContent = styled('div')`
     display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+    > * {
+        margin-bottom: 24px;
+    }
 `;
 
-const ProjectInfo = ({ isOpen, onComplete, newRef }) => {
-    return (
-        <Fieldset ref={newRef}>
-            <Legend isClosed={!isOpen}>Project Info</Legend>
-            <FieldsetContent isOpen={isOpen}>
-                <Input narrow required placeholder="Project Name" />
-                <Input
-                    narrow
-                    required
-                    placeholder="Short description of project"
-                />
-                <Input
-                    narrow
-                    required
-                    placeholder="Tools used (separate each tool with a comma)"
-                />
-                <Input narrow required placeholder="GitHub Link" />
-                <Input narrow placeholder="Other project link" />
-                <Input narrow placeholder="YouTube Video Demo Link" />
+const FormWithMargin = styled('form')`
+    margin: 64px 0;
+`;
+
+const ProjectInfo = ({ isOpen, onComplete, newRef }) => (
+    <Fieldset ref={newRef}>
+        <Legend isClosed={!isOpen}>Project Info</Legend>
+        <FieldsetContent isOpen={isOpen}>
+            <Input narrow required placeholder="Project Name" />
+            <Input narrow required placeholder="Short description of project" />
+            <Input
+                narrow
+                required
+                placeholder="Tools used (separate each tool with a comma)"
+            />
+            <Input narrow required placeholder="GitHub Link" />
+            <Input narrow placeholder="Other project link" />
+            <Input narrow placeholder="YouTube Video Demo Link" />
+            <Centered>
                 <Button primary onClick={onComplete}>
                     Next
                 </Button>
-            </FieldsetContent>
-        </Fieldset>
-    );
-};
+            </Centered>
+        </FieldsetContent>
+    </Fieldset>
+);
 
-const ShareDetails = ({ isOpen, onComplete, newRef }) => {
-    return (
-        <Fieldset isOpen={isOpen} ref={newRef}>
-            <Legend isClosed={!isOpen}>Share Details</Legend>
-            <FieldsetContent isOpen={isOpen}>
-                <TextArea required placeholder="About the Project" />
-                <TextArea required placeholder="Inspiration for the project" />
-                <TextArea required placeholder="Why did you use MongoDB" />
-                <TextArea required placeholder="How it works" />
+const ShareDetails = ({ isOpen, onComplete, newRef }) => (
+    <Fieldset isOpen={isOpen} ref={newRef}>
+        <Legend isClosed={!isOpen}>Share Details</Legend>
+        <FieldsetContent isOpen={isOpen}>
+            <TextArea required placeholder="About the Project" />
+            <TextArea required placeholder="Inspiration for the project" />
+            <TextArea required placeholder="Why did you use MongoDB" />
+            <TextArea required placeholder="How it works" />
+            <Centered>
                 <Button primary onClick={onComplete}>
                     Next
                 </Button>
-            </FieldsetContent>
-        </Fieldset>
-    );
-};
+            </Centered>
+        </FieldsetContent>
+    </Fieldset>
+);
 
-const PromoteYourself = ({ isOpen, onComplete, newRef }) => {
-    return (
-        <Fieldset ref={newRef}>
-            <Legend isClosed={!isOpen}>Promote Yourself</Legend>
-            <FieldsetContent isOpen={isOpen}>
-                <Input required narrow placeholder="First Name" />
-                <Input required narrow placeholder="Last Name" />
-                <Input
-                    required
-                    narrow
-                    type="email"
-                    placeholder="Email Address"
-                />
-                <Input required narrow placeholder="School Name" />
-                <Input narrow placeholder="GitHub Page" />
-                <Input narrow placeholder="Twitter" />
-                <Input narrow placeholder="LinkedIn" />
-                <Input narrow placeholder="Personal Website" />
+const PromoteYourself = ({ isOpen, onComplete, newRef }) => (
+    <Fieldset ref={newRef}>
+        <Legend isClosed={!isOpen}>Promote Yourself</Legend>
+        <FieldsetContent isOpen={isOpen}>
+            <Input required narrow placeholder="First Name" />
+            <Input required narrow placeholder="Last Name" />
+            <Input required narrow type="email" placeholder="Email Address" />
+            <Input required narrow placeholder="School Name" />
+            <Input narrow placeholder="GitHub Page" />
+            <Input narrow placeholder="Twitter" />
+            <Input narrow placeholder="LinkedIn" />
+            <Input narrow placeholder="Personal Website" />
+            <Centered>
                 <Button primary onClick={onComplete}>
                     Finish
                 </Button>
-            </FieldsetContent>
-        </Fieldset>
-    );
-};
+            </Centered>
+        </FieldsetContent>
+    </Fieldset>
+);
 
 const Form = () => {
     const [fieldsetOpen, setFieldsetOpen] = useState([true, false, false]);
@@ -132,7 +142,7 @@ const Form = () => {
     );
 
     return (
-        <form>
+        <FormWithMargin>
             <ProjectInfo
                 isOpen={fieldsetOpen[0]}
                 newRef={fieldsetOneRef}
@@ -148,7 +158,7 @@ const Form = () => {
                 newRef={fieldsetThreeRef}
                 onComplete={onFinalPartComplete}
             />
-        </form>
+        </FormWithMargin>
     );
 };
 
