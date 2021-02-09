2bcf835f97ffdcdaf6862511c45e868415873715
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-02-09 11:18:07 -0500

    Add state to share details section

--- src/components/pages/student-submit/form/share-details.js
+++ src/components/pages/student-submit/form/share-details.js
@@ -2,12 +2,34 @@ import React from 'react';
 import TextArea from '~components/dev-hub/text-area';
 import SubmitFormFieldset from './submit-form-fieldset';
 
-const ShareDetails = ({ ...props }) => (
+const RequiredTextarea = ({ ...props }) => <TextArea required {...props} />;
+
+const ShareDetails = ({ onChange, state, ...props }) => (
     <SubmitFormFieldset buttonText="Next" legendText="Share Details" {...props}>
-        <TextArea required placeholder="About the Project" />
-        <TextArea required placeholder="Inspiration for the project" />
-        <TextArea required placeholder="Why did you use MongoDB" />
-        <TextArea required placeholder="How it works" />
+        <RequiredTextarea
+            name="about_project"
+            onChange={onChange}
+            value={state.about_project}
+            placeholder="About the Project"
+        />
+        <RequiredTextarea
+            name="inspiration"
+            onChange={onChange}
+            value={state.inspiration}
+            placeholder="Inspiration for the project"
+        />
+        <RequiredTextarea
+            name="why_mongodb"
+            onChange={onChange}
+            value={state.why_mongodb}
+            placeholder="Why did you use MongoDB"
+        />
+        <RequiredTextarea
+            name="how_it_works"
+            onChange={onChange}
+            value={state.how_it_works}
+            placeholder="How it works"
+        />
     </SubmitFormFieldset>
 );
 
