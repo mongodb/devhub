c7ded98ac5be9d9a5840988b2911f514d065976c
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-02-10 13:54:08 -0500

    Clean up repeatable form, keys

--- src/hooks/use-student-spotlight-reducer.js
+++ src/hooks/use-student-spotlight-reducer.js
@@ -1,7 +1,7 @@
 import { useReducer } from 'react';
 import { studentSpotlightReducer } from '~utils/student-spotlight-reducer';
 
-const initialState = { students: [{}] };
+const initialState = { students: [{ key: 0 }] };
 
 export const useStudentSpotlightReducer = () => {
     const [state, dispatch] = useReducer(studentSpotlightReducer, initialState);
