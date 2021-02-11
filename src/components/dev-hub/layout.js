21d3aee67a0490b446ada1052e0c205262401725
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-02-11 10:44:52 -0500

    DEVHUB-432: Remove CFP Banner (#653)

--- src/components/dev-hub/layout.js
+++ src/components/dev-hub/layout.js
@@ -11,7 +11,6 @@ import { Helmet } from 'react-helmet';
 import GlobalNav from './global-nav';
 import GlobalFooter from './global-footer';
 import { darkTheme, fontSize, lineHeight, screenSize, size } from './theme';
-import MongodbLiveBanner from './mongodb-live-banner';
 
 import '../../styles/font.css';
 import 'typeface-fira-mono';
@@ -101,7 +100,6 @@ export default ({ children, includeCanonical = true }) => {
                     )}
                 </Helmet>
                 <Global styles={style} />
-                <MongodbLiveBanner />
                 <GlobalNav />
                 <TabProvider>
                     <Main>{children}</Main>
