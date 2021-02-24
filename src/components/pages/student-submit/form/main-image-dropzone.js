3cd5faf4e828685740bf944f835a257332dbaf56
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-02-24 11:48:37 -0500

    Add quick mobile look, begin to prep refactor

--- src/components/pages/student-submit/form/main-image-dropzone.js
+++ src/components/pages/student-submit/form/main-image-dropzone.js
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import { useDropzone } from 'react-dropzone';
-import { H5 } from '~components/dev-hub/text';
+import { H5, P2 } from '~components/dev-hub/text';
 import { layer, screenSize } from '~components/dev-hub/theme';
+import useMedia from '~hooks/use-media';
 
 const DROPZONE_HEIGHT = '64px';
 const DROPZONE_MOBILE_HEIGHT = '48px';
+
+const firaMono = css`
+    font-family: 'Fira Mono';
+`;
 
 const greyText = theme => css`
     color: ${theme.colorMap.greyLightTwo};
 `;
 
 const GreyH5 = styled(H5)`
+    ${({ theme }) => greyText(theme)};
+`;
+
+const GreyP2 = styled(P2)`
     ${({ theme }) => greyText(theme)};
 `;
 
@@ -29,7 +38,7 @@ const Dropzone = styled('div')`
     position: relative;
     text-align: center;
     @media ${screenSize.upToMedium} {
-        height: ${DROPZONE_MOBILE_HEIGHT};
+        min-height: ${DROPZONE_MOBILE_HEIGHT};
     }
 `;
 
@@ -62,7 +71,7 @@ const Image = styled('img')`
 // Adopted from https://react-dropzone.js.org/#section-previews
 const MainImageDropzone = ({ onChange }) => {
     const [file, setFile] = useState(null);
-
+    const isMobile = useMedia(screenSize.upToMedium);
     const onDrop = files => {
         if (files.length) {
             const mainImage = files[0];
@@ -108,6 +117,10 @@ const MainImageDropzone = ({ onChange }) => {
                     <ThumbnailContent>
                         <Image src={file.preview} />
                     </ThumbnailContent>
+                ) : isMobile ? (
+                    <GreyP2 collapse css={firaMono}>
+                        + Add Image
+                    </GreyP2>
                 ) : (
                     <GreyH5 collapse>Drag and drop/upload image</GreyH5>
                 )}
