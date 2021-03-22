a48eb7a637196bf8589cb1a71199ae72d8836b4d
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-03-22 13:08:45 -0400

    DEVHUB-559: Fix Staging Internal Links

--- src/utils/make-link-internal-if-applicable.js
+++ src/utils/make-link-internal-if-applicable.js
@@ -3,18 +3,23 @@ import { addTrailingSlashIfMissing } from './add-trailing-slash-if-missing';
 import { SITE_URL, FORUMS_URL } from '~src/constants';
 import { isLinkForImage } from '~utils/is-link-for-image';
 
-export const makeLinkInternalIfApplicable = link => {
+export const makeLinkInternalIfApplicable = (link, includePrefix = false) => {
     if (!link) {
         return link;
     }
     const linkIncludesDevHub = link.includes(SITE_URL);
     const linkGoesToForums = link.includes(FORUMS_URL);
     if (linkIncludesDevHub && !linkGoesToForums) {
+        const linkWithoutSiteUrl = link.replace(SITE_URL, '');
         // Forums is technically "external" from an app standpoint, so we leave
         // that one alone
         return isLinkForImage(link)
-            ? withPrefix(link.replace(SITE_URL, ''))
-            : addTrailingSlashIfMissing(link.replace(SITE_URL, ''));
+            ? withPrefix(linkWithoutSiteUrl)
+            : addTrailingSlashIfMissing(
+                  includePrefix
+                      ? withPrefix(linkWithoutSiteUrl)
+                      : linkWithoutSiteUrl
+              );
     }
     return link;
 };
