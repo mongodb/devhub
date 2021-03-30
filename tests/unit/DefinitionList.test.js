fd228edb86604f125580cd2176c834129ea76bc2
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-03-30 12:10:05 -0400

    DEVHUB-107: Update literal style

--- tests/unit/DefinitionList.test.js
+++ tests/unit/DefinitionList.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from 'emotion-theming';
 import { render } from 'enzyme';
 import DefinitionList from '../../src/components/DefinitionList';
+import { darkTheme } from '../../src/components/dev-hub/theme';
 import mockData from './data/DefinitionList.test.json';
 
 it('DefinitionList renders correctly', () => {
-    const tree = render(<DefinitionList nodeData={mockData} />);
+    const tree = render(
+        <ThemeProvider theme={darkTheme}>
+            <DefinitionList nodeData={mockData} />
+        </ThemeProvider>
+    );
     expect(tree).toMatchSnapshot();
 });
