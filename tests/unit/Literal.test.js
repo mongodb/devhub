183f450f42f3280189b65ec0d6e2b4f20f249925
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-03-30 12:51:24 -0400

    DEVHUB-107: Update literal style (#755)

--- tests/unit/Literal.test.js
+++ tests/unit/Literal.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'emotion-theming';
 import { shallow } from 'enzyme';
 import Literal from '../../src/components/Literal';
+import { darkTheme } from '../../src/components/dev-hub/theme';
 
 // data for this component
 import mockData from './data/Literal.test.json';
 
 it('renders correctly', () => {
-    const tree = shallow(<Literal nodeData={mockData} />);
+    const tree = shallow(
+        <ThemeProvider theme={darkTheme}>
+            <Literal nodeData={mockData} />
+        </ThemeProvider>
+    );
     expect(tree).toMatchSnapshot();
 });
