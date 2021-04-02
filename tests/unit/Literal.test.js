726c34521d63436870b66eb495d5b77570603c78
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-04-02 10:46:45 -0400

    Update snapshots for emotion 11

--- tests/unit/Literal.test.js
+++ tests/unit/Literal.test.js
@@ -8,10 +8,6 @@ import { darkTheme } from '../../src/components/dev-hub/theme';
 import mockData from './data/Literal.test.json';
 
 it('renders correctly', () => {
-    const tree = shallow(
-        <ThemeProvider theme={darkTheme}>
-            <Literal nodeData={mockData} />
-        </ThemeProvider>
-    );
+    const tree = shallow(<Literal nodeData={mockData} />);
     expect(tree).toMatchSnapshot();
 });
