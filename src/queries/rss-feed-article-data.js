a793ac66e45ea33d4019be3204187b58c599a91b
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-04-23 13:17:40 -0400

    Add strapi articles to RSS feed

--- src/queries/rss-feed-article-data.js
+++ src/queries/rss-feed-article-data.js
@@ -1,5 +1,14 @@
 const rssFeedArticleData = `
     query RSSFeedArticleData {
+        allStrapiArticles(sort: {fields: published_at, order: DESC}) {
+          nodes {
+            description
+            slug
+            pubdate: published_at
+            title: name
+            type
+          }
+        }
         allArticle(sort: {fields: pubdate, order: DESC}) {
             nodes {
                 slug: id
