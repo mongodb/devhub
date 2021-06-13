04ac71436e619c86f196f15e288e46ba2bc697ee
Harika Kotipalli <harika.kotipalli@Harikas-MacBook-Pro.local>
2021-06-13 18:54:48 -0400

    DEVHUB-606 Adding new learn tabs component

---
+++ src/components/dev-hub/learn-tabs.js
@@ -0,0 +1,118 @@
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Tabs as LeafyTabs, Tab } from '@leafygreen-ui/tabs';
+import Button from './button';
+import { TabContext } from './tab-context';
+
+const TabButtonStyle = () => css`
+    border-radius: 0;
+    :active,
+    :hover,
+    :focus {
+        color: ${({ theme }) => theme.colorMap.darkGreen};
+        :after {
+            background-color: ${({ theme }) => theme.colorMap.darkGreen};
+        }
+    }
+`;
+
+const StyledTabButton = styled(Button)`
+    ${TabButtonStyle}
+`;
+
+const hiddenTabStyling = () => css`
+    & > div:first-child {
+        display: none;
+    }
+`;
+
+const TabsStyle = () => css`
+    ${({ ishidden }) => ishidden && hiddenTabStyling};
+    button[aria-selected='true'] {
+        color: ${({ theme }) => theme.colorMap.darkGreen};
+        :after {
+            background-color: ${({ theme }) => theme.colorMap.darkGreen};
+        }
+    }
+`;
+
+const StyledTabs = styled(LeafyTabs)`
+    ${TabsStyle}
+`;
+
+// Name anonymous tabsets by alphabetizing their tabids and concatenating with a forward slash
+const generateAnonymousTabsetName = tabIds => [...tabIds].sort().join('/');
+
+const TabButton = ({ ...props }) => (
+    <StyledTabButton renderAsButton {...props} />
+);
+
+const Tabs = ({ tabsList, handleClick, activeItem }) => {
+    const { activeTabs, setActiveTab } = useContext(TabContext);
+    const tabsetName = generateAnonymousTabsetName(tabsList);
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+    /*
+    to set the value from tab context in nav bar
+    */
+    useEffect(() => {
+        setActiveTab({
+            tabset: tabsetName,
+            value: activeItem,
+        });
+    }, [activeItem]);
+
+    /*
+    to set the active tab index if
+    1. setActiveTab triggered from context changes in the nav bar
+    2. setActiveTab triggered from using call back on click
+    */
+    useEffect(() => {
+        const index = tabsList.indexOf(activeTabs[tabsetName]);
+        if (index !== -1) {
+            setActiveTabIndex(index);
+        }
+    }, [activeTabs, tabsList]);
+
+    /*
+    to set the active item upon user click on any leafy tabs
+    */
+    const onClick = useCallback(
+        index => {
+            const tabValue = tabsList[index];
+            setActiveTab({
+                tabset: tabsetName,
+                value: tabValue,
+            });
+            handleClick(tabValue);
+        },
+        [tabsList]
+    );
+
+    return (
+        <>
+            <StyledTabs
+                as={TabButton}
+                darkMode
+                data-test="Tabs"
+                selected={activeTabIndex}
+                setSelected={onClick}
+            >
+                {tabsList.map((tab, index) => {
+                    const tabId = tab;
+                    return (
+                        <Tab
+                            data-tabid={tabId}
+                            role="tab"
+                            key={index}
+                            name={tabId}
+                        ></Tab>
+                    );
+                })}
+            </StyledTabs>
+        </>
+    );
+};
+
+export default Tabs;
