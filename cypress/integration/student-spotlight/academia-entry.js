3c389fdfd86dd4de0ce2e88c0a5a141af6ee7249
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-06-21 14:59:07 -0400

    Add some logic to check entry page

--- cypress/integration/student-spotlight/academia-entry.js
+++ cypress/integration/student-spotlight/academia-entry.js
@@ -4,7 +4,26 @@ describe('Academia Entry Page', () => {
     it('should render the entry page', () => {
         cy.visit(ENTRY_URL);
     });
-    it('should have a header', () => {
-        cy.contains('MongoDB for Academia');
+    it('should link to other locations', () => {
+        cy.get('main').within(() => {
+            cy.get('a')
+                .first()
+                .should('have.attr', 'href')
+                .and('equal', 'https://www.mongodb.com/students');
+            cy.get('a')
+                .eq(1)
+                .should('have.attr', 'href')
+                .and('equal', '/academia/educators/');
+            cy.get('a')
+                .last()
+                .should('have.attr', 'href')
+                .and('equal', '/academia/students/');
+        });
+    });
+    it('should have a project grid', () => {
+        cy.get('[data-test="project-card"]')
+            .first()
+            .should('have.attr', 'href')
+            .and('eq', '/project/go-fifa/');
     });
 });
