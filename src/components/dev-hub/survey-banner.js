72a1549391be9237b66aa3626a42df5914d8b7e7
harika.kotipalli <harika.1818@gmail.com>
2021-07-07 20:11:58 -0400

    DEVHUB-771 fixing css issues from PR

--- src/components/dev-hub/survey-banner.js
+++ src/components/dev-hub/survey-banner.js
@@ -1,57 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import Button from './button';
-import Icon from '@leafygreen-ui/icon';
-import { fontSize, screenSize, size, colorMap } from './theme';
+import { fontSize, screenSize, size, layer } from './theme';
+import XWithCircleIcon from '@leafygreen-ui/icon/dist/XWithCircle';
+import IconButton from '@leafygreen-ui/icon-button';
 
 const Footer = styled('div')`
-    background-color: ${colorMap.greyDarkTwo};
+    background-color: ${({ theme }) => theme.colorMap.greyDarkTwo};
     position: fixed;
     left: 0;
     right: 0;
     bottom: 0;
-    border: 1px solid ${colorMap.greyDarkOne};
-`;
-
-const FooterContent = styled('div')`
+    border: 1px solid ${({ theme }) => theme.colorMap.greyDarkOne};
+    padding: 0px ${size.medium};
+    @media ${screenSize.mediumAndUp} {
+        padding: 0px ${size.large};
+    }
     display: flex;
     justify-content: space-between;
-    @media ${screenSize.mediumAndUp} {
-        margin: 0 2% 0 15%;
-    } ;
+    z-index: ${layer.superFront};
 `;
 
 const SurveyContent = styled('div')`
-    @media ${screenSize.mediumAndUp} {
+    margin: auto;
+    @media ${screenSize.largeAndUp} {
         display: flex;
+        flex-wrap: wrap;
     }
 `;
 
-const SurveyText = styled('div')`
+const SurveyText = styled('P')`
     font-family: 'Fira Mono', monospace;
     font-size: ${fontSize.tiny};
-    padding: 18px;
-    @media ${screenSize.upToMedium} {
-        padding: ${size.xsmall} ${size.xsmall} 0px ${size.xsmall};
-    }
+    padding-left: ${size.small};
 `;
 
 const SurveyButtonContainer = styled('div')`
-    display: 'flex';
-    padding: ${size.xsmall} 0px ${size.xsmall} ${size.xsmall};
+    display: flex;
+    padding: ${size.xsmall};
 `;
 
 const CloseButtonContainer = styled('div')`
-    padding: 18px;
+    padding: ${size.small};
 `;
 
 const StyledButton = styled(Button)`
     font-size: ${fontSize.tiny};
     padding: ${size.default} ${size.medium};
-`;
-
-const StyledIcon = styled(Icon)`
-    cursor: pointer;
 `;
 
 const surveyLink =
@@ -75,35 +70,30 @@ const SurveyBanner = () => {
 
     return showSurveyBanner === true ? (
         <Footer>
-            <FooterContent>
-                <SurveyContent>
-                    <SurveyText>
-                        Help improve this experience by taking a 2 minute
-                        survey!
-                    </SurveyText>
-                    <SurveyButtonContainer>
-                        <StyledButton
-                            onClick={closeSurveyBanner}
-                            primary
-                            hasArrow={false}
-                            href={surveyLink}
-                            target="_blank"
-                        >
-                            Take Survey
-                        </StyledButton>
-                        <StyledButton onClick={closeSurveyBanner}>
-                            No Thanks
-                        </StyledButton>
-                    </SurveyButtonContainer>
-                </SurveyContent>
-                <CloseButtonContainer>
-                    <StyledIcon
-                        glyph="XWithCircle"
-                        fill={colorMap.devWhite}
+            <SurveyContent>
+                <SurveyText>
+                    Help improve this experience by taking a 2 minute survey!
+                </SurveyText>
+                <SurveyButtonContainer>
+                    <StyledButton
                         onClick={closeSurveyBanner}
-                    />
-                </CloseButtonContainer>
-            </FooterContent>
+                        primary
+                        hasArrow={false}
+                        href={surveyLink}
+                        target="_blank"
+                    >
+                        Take Survey
+                    </StyledButton>
+                    <StyledButton onClick={closeSurveyBanner}>
+                        No Thanks
+                    </StyledButton>
+                </SurveyButtonContainer>
+            </SurveyContent>
+            <CloseButtonContainer>
+                <IconButton onClick={closeSurveyBanner}>
+                    <XWithCircleIcon fill="#f9fbfa" />
+                </IconButton>
+            </CloseButtonContainer>
         </Footer>
     ) : (
         <></>
