25962b03c4b25aa8a2483f725eda402d396e07a8
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-08-03 11:59:43 -0400

    Revert "Revert "Deploy: 3 August 2021""

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,9 +64,8 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-sitemap',
             options: {
-                output: '/sitemap.xml',
                 // Exclude paths we are using the noindex tag on
-                exclude: [
+                excludes: [
                     '/language/*',
                     '/product/*',
                     '/storybook ',
--- src/utils/get-series-articles.js
+++ src/utils/get-series-articles.js
@@ -1,14 +1,18 @@
+import { fuzzySlugMatch } from './fuzzy-slug-match';
+
 // given a page slug, find all occurrances in "allSeries"
-const getSeriesArticles = (allSeries, slug) => {
-    if (!allSeries || !slug) return [];
-    const series = Object.keys(allSeries);
-    const seriesForThisArticle = {};
-    series.forEach(s => {
-        if (allSeries[s].includes(slug)) {
-            seriesForThisArticle[s] = allSeries[s];
+export const getSeriesArticles = (allSeries, targetSlug) => {
+    if (!allSeries || !targetSlug) return [];
+    const seriesForThisArticle = [];
+    const addSeriesIfContainsSlug = (series, slug) => {
+        if (fuzzySlugMatch(targetSlug, slug)) {
+            seriesForThisArticle.push(series);
         }
-    });
+    };
+    allSeries.forEach(series =>
+        series.articles.forEach(({ slug: slugInSeries }) =>
+            addSeriesIfContainsSlug(series, slugInSeries)
+        )
+    );
     return seriesForThisArticle;
 };
-
-module.exports.getSeriesArticles = getSeriesArticles;
