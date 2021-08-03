8c13f0c6d293f95f92d5400b1e82a403f233ca73
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-08-03 16:22:17 -0400

    Add rule to swap okta to js/auto

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -238,6 +238,15 @@ export const onCreateWebpackConfig = ({ stage, loaders, actions }) => {
                 ),
             },
         },
+        module: {
+            rules: [
+                {
+                    test: /\.js/,
+                    include: /node_modules\/@okta\/okta-auth-js/,
+                    type: 'javascript/auto',
+                },
+            ],
+        },
     });
 };
 
