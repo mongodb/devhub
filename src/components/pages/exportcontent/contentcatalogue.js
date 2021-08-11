901935dc3090922904b811b416c4ffc3907bdeee
harika.kotipalli <harika.1818@gmail.com>
2021-08-11 17:57:41 -0400

    DEVHUB-794 adding export page

--- src/components/pages/exportcontent/contentcatalogue.js
+++ src/components/pages/exportcontent/contentcatalogue.js
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from '@reach/router';
-import queryString from 'query-string';
-import { parseQueryString } from '~utils/query-string';
+import { buildQueryString, parseQueryString } from '~utils/query-string';
 
-const URL = `https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/devhub-search-service-dxfcg/service/devhub_export/incoming_webhook/devhubexport`;
+const SIMPLESEARCHURL = `https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/devhub-search-service-dxfcg/service/devhub_export/incoming_webhook/simplesearch`;
+const COMPOSITESEARCHURL = `https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/devhub-search-service-dxfcg/service/devhub_export/incoming_webhook/compositesearch`;
 
 const ContentCatalogue = () => {
     const { search } = useLocation();
     const { ...params } = parseQueryString(search);
-    const { content, text } = params;
+    const queryParams = buildQueryString(params);
+    const urlEndPoint =
+        'content' in params ? SIMPLESEARCHURL : COMPOSITESEARCHURL;
     const [searchResult, setSearchResult] = useState(null);
     useEffect(() => {
-        const url = `${URL}?content=${content}&text=${text}`;
+        const url = `${urlEndPoint}${queryParams}`;
         fetch(url)
             .then(response => response.json())
             .then(data => setSearchResult(data));
     }, []);
-    return searchResult && <div>{JSON.stringify(searchResult)}</div>;
+    return (
+        searchResult && (
+            <div>
+                <pre>{JSON.stringify(searchResult, null, 2)}</pre>
+            </div>
+        )
+    );
 };
 
 export default ContentCatalogue;
