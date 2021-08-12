315645e7cd1405cf4465089adc788659c0d292e2
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-08-12 17:23:20 -0400

    DEVHUB-801: Filter Draft Series Articles from Prod Platform Builds

--- src/classes/strapi-article-series.ts
+++ src/classes/strapi-article-series.ts
@@ -6,37 +6,48 @@ import dlv from 'dlv';
 import { ArticleSeries, SeriesArticle } from '../interfaces/article-series';
 import { transformArticleStrapiData } from '../utils/transform-article-strapi-data';
 
+const STRAPI_COMPONENT_TYPE = 'article-info.strapi-article';
+
 export class StrapiArticleSeries implements ArticleSeries {
     articles: SeriesArticle[];
     title: String;
     constructor(title, entries, snootyTitleMapping) {
-        this.articles = entries.map(article => {
-            const isStrapi = !!article.article;
-            // Strapi's API for articles is different from what Snooty provides
-            return isStrapi
-                ? this.handleStrapiArticle(article.article)
-                : this.handleSnootyArticle(article, snootyTitleMapping);
-        });
+        this.articles = entries
+            .map(article => {
+                if (!article) return null;
+                const isStrapi =
+                    article.strapi_component === STRAPI_COMPONENT_TYPE;
+                // Strapi's API for articles is different from what Snooty provides
+                return isStrapi
+                    ? this.handleStrapiArticle(article.article)
+                    : this.handleSnootyArticle(article, snootyTitleMapping);
+            })
+            // Remove nulls
+            .filter(entry => !!entry);
         this.title = title;
     }
 
-    handleSnootyArticle = (article, snootyTitleMapping) => ({
-        slug: article.slug,
-        title: dlv(
-            snootyTitleMapping,
-            [
-                // Remove any leading/trailing slashes for use with Snooty's provided mapping
-                article.slug.replace(/^\/|\/$/g, ''),
-                'query_fields',
-                'title',
-                0,
-                'value',
-            ],
-            article.slug
-        ),
-    });
+    handleSnootyArticle = (article, snootyTitleMapping) => {
+        if (!article.slug) return null;
+        return {
+            slug: article.slug,
+            title: dlv(
+                snootyTitleMapping,
+                [
+                    // Remove any leading/trailing slashes for use with Snooty's provided mapping
+                    article.slug.replace(/^\/|\/$/g, ''),
+                    'query_fields',
+                    'title',
+                    0,
+                    'value',
+                ],
+                article.slug
+            ),
+        };
+    };
 
     handleStrapiArticle = article => {
+        if (!article.article) return null;
         const { name, slug } = transformArticleStrapiData(article);
         return { slug, title: name };
     };
