3882a4075e9ca3a729ebc509b6f696bde4792311
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-08-17 13:24:33 -0400

    Handle drafts

--- src/utils/setup/get-featured-articles-from-graphql.js
+++ src/utils/setup/get-featured-articles-from-graphql.js
@@ -2,6 +2,15 @@ import dlv from 'dlv';
 import { featuredArticles } from '../../queries/featured-articles';
 import { transformArticleStrapiData } from '../transform-article-strapi-data';
 
+const STRAPI_COMPONENT_TYPE = 'article-info.strapi-article';
+
+const mapFeaturedArticle = article =>
+    article.strapi_component === STRAPI_COMPONENT_TYPE
+        ? article.article
+            ? transformArticleStrapiData(article.article).slug
+            : null
+        : article.slug;
+
 export const getFeaturedArticlesFromGraphql = async graphql => {
     const data = await graphql(featuredArticles);
     const homePageFeaturedArticles = dlv(
@@ -11,7 +20,9 @@ export const getFeaturedArticlesFromGraphql = async graphql => {
         // This isn't quite polymorphic, since for Strapi a slug by default excludes the
         // type since it is redundant with the type field
         // The case is snooty first and then Strapi second
-    ).map(a => a.slug || transformArticleStrapiData(a.article).slug);
+    )
+        .map(mapFeaturedArticle)
+        .filter(a => !!a);
 
     const learnPageFeaturedArticles = dlv(
         data,
@@ -20,6 +31,8 @@ export const getFeaturedArticlesFromGraphql = async graphql => {
         // This isn't quite polymorphic, since for Strapi a slug by default excludes the
         // type since it is redundant with the type field
         // The case is snooty first and then Strapi second
-    ).map(a => a.slug || transformArticleStrapiData(a.article).slug);
+    )
+        .map(mapFeaturedArticle)
+        .filter(a => !!a);
     return { homePageFeaturedArticles, learnPageFeaturedArticles };
 };
