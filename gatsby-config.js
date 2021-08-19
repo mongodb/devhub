e48fb761aecb30ddf59f8cb69efeabd22735987b
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-08-19 17:06:51 -0400

    Add comment to explain

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,6 +89,9 @@ module.exports = {
                 resolveSiteUrl: () => 'https://developer.mongodb.com/',
             },
         },
+        // We want to omit GTM for testing purposes (not on any production builds)
+        // This process of using the spread operator lets us simply add nothing to the
+        // plugins array if we don't want to use this plugin
         ...(process.env.DISABLE_GTM
             ? []
             : [
