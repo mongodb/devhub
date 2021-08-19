78d27356b5939d9e2c263a4402d82e7ee1a3e0ad
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-08-19 13:48:48 -0400

    Add a naive approach on the front-end

--- src/utils/setup/find-articles-from-slugs.js
+++ src/utils/setup/find-articles-from-slugs.js
@@ -1,19 +1,30 @@
 import { fuzzySlugMatch } from '../fuzzy-slug-match';
 
 export const findArticleWithSlug = (allArticles, slug) =>
-    allArticles.find(article => fuzzySlugMatch(article.slug, slug));
+    allArticles.find(
+        article => !!article && fuzzySlugMatch(article.slug, slug)
+    );
 
 export const findArticlesFromSlugs = (allArticles, articleSlugs, maxSize) => {
     const result = [];
+    if (maxSize) {
+        articleSlugs.length = maxSize;
+    }
+    const articleIterator = allArticles
+        .sort((a, b) => new Date(b.publishedDate) - new Date(a.publishedDate))
+        [Symbol.iterator]();
     // If maxSize is undefined, this will return a shallow copy of articleSlugs
-    articleSlugs.slice(0, maxSize).forEach((featuredSlug, i) => {
+    articleSlugs.forEach(featuredSlug => {
         const targetArticle = findArticleWithSlug(allArticles, featuredSlug);
         if (targetArticle) {
             result.push(targetArticle);
         } else {
             // This article was not found. pick an existing article and add it instead.
-            result.push(allArticles[i]);
+            const mostRecentUnfeaturedArticle = articleIterator.next().value;
+            // We should check this is not yet featured
+            result.push(mostRecentUnfeaturedArticle);
         }
     });
+    // Fill result with the rest
     return result;
 };
