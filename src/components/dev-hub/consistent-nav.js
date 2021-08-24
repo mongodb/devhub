cbfeae4b71bb62f2148dce630d475139d7e4be47
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-08-24 16:14:21 -0400

    Make the nav sticky

--- src/components/dev-hub/consistent-nav.js
+++ src/components/dev-hub/consistent-nav.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { UnifiedNav } from '@mdb/consistent-nav';
-import { layer, screenSize } from '~components/dev-hub/theme';
+import { layer } from '~components/dev-hub/theme';
 
-const CONSISTENT_NAV_DESKTOP_HEIGHT = '88px';
-const CONSISTENT_NAV_MOBILE_HEIGHT = '56px';
-
-const NavWrapper = styled('div')`
-    height: ${CONSISTENT_NAV_DESKTOP_HEIGHT};
+const FrontLayeredNav = styled(UnifiedNav)`
     z-index: ${layer.superFront};
-    @media ${screenSize.upToLarge} {
-        height: ${CONSISTENT_NAV_MOBILE_HEIGHT};
-    }
 `;
 
 const ConsistentNav = () => (
-    <NavWrapper>
-        <UnifiedNav position="relative" property="DEVHUB" />
-    </NavWrapper>
+    <FrontLayeredNav position="sticky" property="DEVHUB" />
 );
 
 export default ConsistentNav;
