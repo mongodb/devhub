a7a1ad1e92d8d193a2f99b1fc92d78cc4f525222
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-09-22 10:47:33 -0400

    Exclude non-approved srcs

--- src/components/dev-hub/raw-html.js
+++ src/components/dev-hub/raw-html.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import sanitizeHtml from 'sanitize-html';
 
+// To allow additional embeds/iframes, update these arrays based on inclusion tag
+// (iframe vs script)
+const ALLOWED_IFRAME_HOSTNAMES = ['open.spotify.com'];
+const ALLOWED_SCRIPT_SRCS = [
+    'https://platform.twitter.com/widgets.js',
+    'https://www.tiktok.com/embed.js',
+];
+
 const isInlineElement = html => html === '<br />';
 
 const RawHTML = ({ nodeData }) => {
@@ -13,25 +21,33 @@ const RawHTML = ({ nodeData }) => {
             sanitizeHtml(value, {
                 allowedTags: sanitizeHtml.defaults.allowedTags.concat([
                     'img',
+                    'iframe',
                     'script',
                 ]),
                 // Need to lock script srcs to include tiktok, spotify, twitter
                 allowedAttributes: false,
+                allowedIframeHostnames: ALLOWED_IFRAME_HOSTNAMES,
                 allowVulnerableTags: true,
+                exclusiveFilter: frame => {
+                    return (
+                        frame.tag === 'script' &&
+                        ALLOWED_SCRIPT_SRCS.includes(frame.src)
+                    );
+                },
             }),
         [value]
     );
+    // We would like script tags to load in external JS in this case
     useEffect(() => {
         if (contentRef && contentRef.current && !hasRenderedScript) {
             const fragment = document
                 .createRange()
                 .createContextualFragment(sanitizedHTML);
             contentRef.current.appendChild(fragment);
+            // This prevents double-addition of the script tags globally
             setHasRenderedScript(true);
-            // Need to prevent double-addition of the script tags globally
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [hasRenderedScript, sanitizedHTML]);
     if (containsScript) {
         return <div ref={contentRef} />;
     } else if (isInlineElement(sanitizedHTML)) {
