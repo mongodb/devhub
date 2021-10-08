fbaeea9b0156ba8ff7e52fed7d4e504642112d2f
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-10-08 14:39:24 -0400

    DEVHUB-847: Resolve jumping logo on consistent nav
    Use small font size

--- src/components/dev-hub/consistent-nav.js
+++ src/components/dev-hub/consistent-nav.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { UnifiedNav } from '@mdb/consistent-nav';
-import { layer } from '~components/dev-hub/theme';
+import { layer, fontSize } from '~components/dev-hub/theme';
 
 export const NAV_DESKTOP_HEIGHT = '88px';
 export const NAV_MOBILE_HEIGHT = '55px';
 
 const FrontLayeredNav = styled(UnifiedNav)`
     z-index: ${layer.superFront};
+    /* The logo relies on consistent font-size, which is to be 16px */
+    /* The value we pull in from the nav computes to 18px (which is our default) */
+    div:first-of-type > a {
+        font-size: ${fontSize.small};
+    }
 `;
 
 const ConsistentNav = () => (
