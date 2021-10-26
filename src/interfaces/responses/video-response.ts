ec322468f1b7d0f9dd42971aefca865346f98a50
harika.kotipalli <harika.1818@gmail.com>
2021-10-26 10:43:55 -0400

    DEVHUB-883 modifying youtube and twitch video object structures to point to strapi version

---
+++ src/interfaces/responses/video-response.ts
@@ -0,0 +1,13 @@
+export interface VideoResponse {
+    description: string;
+    mediaType: string;
+    originalPublishDate: string;
+    slug: string;
+    thumbnailUrl: string;
+    title: string;
+    videoId: string;
+    SEO: object;
+    tags: object[];
+    products: object[];
+    languages: object[];
+}
