bc2cee259893dc9bd4c1750729570116a2913809
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-11-02 12:24:53 -0400

    Initial parity script to wrap sitediff

---
+++ scripts/parity-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Install sitediff if it doesn't exist
+if ! command -v sitediff &> /dev/null
+then
+    echo "sitediff could not be found, please install to your system and add to PATH"
+    exit
+fi
+if [ $# -lt 2 ]; 
+    then echo "requires two sites to check diff and optional full slug of article"
+    exit 2
+fi
+# Take in two URLs
+sitediff init $1 $2
+# Compare Articles
+sitediff diff --cached=none --paths $3
+# Report diff
