ef07d477f945e4b6ec8f01bf8d50dc4134e2770c
Chukwurado Umeaka <chukwurado.umeaka98@gmail.com>
2021-11-19 16:31:28 -0500

    Update search-results.ts

--- src/classes/base/search-results.ts
+++ src/classes/base/search-results.ts
@@ -22,9 +22,9 @@ export class SearchResult {
         this.publishDate = result.pubDate;
         this.slug = result.slug;
         this.title = dlv(result.title, [0, 'value'], result.slug);
-        this.languages = mapTagTypeToUrl(result.languages, 'language');
-        this.products = mapTagTypeToUrl(result.products, 'product');
-        this.tags = mapTagTypeToUrl(result.tags, 'tag');
+        this.languages = mapTagTypeToUrl(result.languages, 'language', true);
+        this.products = mapTagTypeToUrl(result.products, 'product', true);
+        this.tags = mapTagTypeToUrl(result.tags, 'tag', true);
         this.authors = result.authors && result.authors.map((a) =>
             new StrapiAuthor({name: a['name'], bio: a['bio'], location: a['location'], image: a['image']['url'], title: a['title']}));
         this.score = result.score
