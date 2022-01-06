25157a5b618111f41a96362c0a8055c456773230
harika.kotipalli <harika.1818@gmail.com>
2022-01-06 15:16:16 -0500

    Adding snooty user

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -44,7 +44,7 @@ const pluralizeIfNeeded = {
 const metadata = getMetadata();
 
 const DB = metadata.database;
-const PAGE_ID_PREFIX = `${metadata.project}/${metadata.snootyUser}/${metadata.parserBranch}`;
+let PAGE_ID_PREFIX = `${metadata.project}/${metadata.snootyUser}/${metadata.parserBranch}`;
 if (!Boolean(process.env.GATSBY_PREVIEW_MODE)) {
     PAGE_ID_PREFIX = `${metadata.project}/${metadata.parserUser}/${metadata.parserBranch}`;
 }
